f470c7fccccc3fbd093ec5358ca334554e7fb51e
Jared Williams <williams@nysenate.gov>
2011-03-11 10:48:47 -0500

    migrating to maven structure

--- src/gov/nysenate/sage/model/abstracts/AbstractGeocoder.java
+++
@@ -1,8 +0,0 @@
-package gov.nysenate.sage.model.abstracts;
-
-import gov.nysenate.sage.model.Point;
-
-public abstract interface AbstractGeocoder {
-	public Point doParsing(String address);
-	public String constructUrl(String address);
-}
