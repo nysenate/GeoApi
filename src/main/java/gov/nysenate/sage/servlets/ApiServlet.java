3759f577e656eaa043b5ae1dabb98fb44184eb51
Graylin Kim <graylin.kim@gmail.com>
2012-08-23 12:44:12 -0400

    Remove Resource.init() and fix whitespace.

--- src/main/java/gov/nysenate/sage/servlets/ApiServlet.java
+++ src/main/java/gov/nysenate/sage/servlets/ApiServlet.java
@@ -34,160 +34,161 @@ import com.thoughtworks.xstream.XStream;
 import com.thoughtworks.xstream.io.xml.DomDriver;
 
 public class ApiServlet extends HttpServlet {
-	private Logger logger = Logger.getLogger(ApiServlet.class);
-	
-	private static final long serialVersionUID = 1L;
-	
-	HashMap<String,ApiMethod> methods = null;
-	ApiController control = null;		
-	
-	public void init(ServletConfig config) throws ServletException {
-		logger.info("Initializing ApiServlet");
-		
-		super.init(config);
-		Resource.init(this);
-		methods = MethodLoader.getMethods();
-		control = new ApiController();
-		
-	}
+    private final Logger logger = Logger.getLogger(ApiServlet.class);
 
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		Resource.init(this);
-		
-		PrintWriter out = response.getWriter();
+    private static final long serialVersionUID = 1L;
 
-		String uri = java.net.URLDecoder.decode(request.getRequestURI(),"utf-8");
-		
-		logger.info("Request URI: " + uri);
-		
-		uri = uri.toLowerCase().replaceAll("(/geoapi/|api/)", "");
-		
-		StringTokenizer stok = new StringTokenizer(uri,"/");
-		
-		String key = request.getParameter("key");
-		String format = null;
-		String command = null;
-		String type = null;
-				
-		try {
-			ApiUser user = null;
-			if(key == null)
-				user = control.getUser(Resource.get("user.default"));
-			else
-				user = control.getUser(key);
-			if(user == null)
-				throw new ApiAuthenticationException();
-			
-			format = stok.nextToken();
-			command = stok.nextToken();
-			type = stok.nextToken();
-			
-			ApiMethod method = null;
-			if((method = methods.get(command)) != null ) {
-				if(!method.validFormat(format))
-					throw new ApiFormatException(format);
-				
-				if(!method.validType(type))
-					throw new ApiTypeException(type);
-				
-				AbstractApiExecute executionClass = method.getInstanceOfExecutionClass();
-				Object obj = executionClass.execute(request, response, getMore(format, type, stok));
-				
-				if(obj == null) {
-					throw new ApiInternalException();
-				}
-				else {
-					if(format.equals("xml")) {
-						out.print(executionClass.toXml(obj, method.getXstreamClasses()));
-					}
-					else if(format.equals("json")) {
-						out.print(executionClass.toJson(obj));
-					}
-					else {
-						out.print(executionClass.toOther(obj, format));
-					}
-				}
+    HashMap<String,ApiMethod> methods = null;
+    ApiController control = null;
 
-				
-				if(method.getWriteMetric()) {
-					logger.info("writing metric");
-					
-					control.addMetric(user.getId(), constructUrl(uri, request), request.getRemoteAddr());
-				}
-			}
-			else {
-				throw new ApiCommandException(command);
-			}
-		}
-		catch (ApiCommandException ace) {
-			out.write(getError("error", "Invalid command: " + ace.getMessage() 
-					+ ", please view API documentation.", format));
-			logger.warn(ace);
-		}
-		catch (ApiFormatException afe) {
-			out.write(getError("error", "Invalid format: " + afe.getMessage()  +
-					" for command: " + command + ", please review API documentation.", format));
-			logger.warn(afe);
-		}
-		catch (ApiTypeException ate) {
-			out.write(getError("error", "Invalid input " + ate.getMessage() 
-					+ " for command: " + command +", please review API documentation.", format));
-			logger.warn(ate);
-		}
-		catch (ApiAuthenticationException aae) {
-			out.write(getError("error", "Could not be authorized.", format));
-			logger.warn(aae);
-		}
-		catch (Exception e) {
-			out.write(getError("error", "Invalid request " + constructUrl(uri, request) 
-					+ ", please check that your input is properly formatted " +
-							"and review the API documentation.", format));
-			logger.warn(e);
-		}
-	}
-	
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		doGet(request, response);
-	}
-	
-	public String getError(String name, String reason, String format) {
-		logger.warn("Error! name: " + name + ", reason: " + reason);
-		
-		ErrorResponse e = new ErrorResponse(reason);
-		if(format != null && format.matches("xml|kml")) {
-			XStream xstream = new XStream(new DomDriver());
-			xstream.processAnnotations(new Class[]{ValidateResponse.class, ErrorResponse.class});
-			return xstream.toXML(e);
-		}
-		else if(format != null && format.matches("json")) {
-			Gson gson = new Gson();
-			return gson.toJson(e);
-		}
-		else {
-			return "ERROR," + reason.replaceAll(",", "");
-		}
-		
-	}
-	
-	public String constructUrl(String uri, HttpServletRequest req) {
-    	String ret = uri + "?";
-    	Enumeration<?> params = req.getParameterNames();
-    	while(params.hasMoreElements()) {
-    		String elem = (String)params.nextElement();
-    		ret += elem + "=" + (String)req.getParameter(elem) + "&";
-    	}
-    	return ret.replaceAll("&$", "");
+    @Override
+    public void init(ServletConfig config) throws ServletException {
+        logger.info("Initializing ApiServlet");
+
+        super.init(config);
+        methods = MethodLoader.getMethods();
+        control = new ApiController();
+
     }
-	
-	public ArrayList<String> getMore(String format, String type, StringTokenizer stok) {
-		ArrayList<String> strings = new ArrayList<String>();
-		strings.add(format);
-		strings.add(type);
-		while(stok.hasMoreTokens()) {
-			strings.add(stok.nextToken());
-		}
-		return strings;
-	}
 
-	
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        PrintWriter out = response.getWriter();
+
+        String uri = java.net.URLDecoder.decode(request.getRequestURI(),"utf-8");
+
+        logger.info("Request URI: " + uri);
+
+        uri = uri.toLowerCase().replaceAll("(/geoapi/|api/)", "");
+
+        StringTokenizer stok = new StringTokenizer(uri,"/");
+
+        String key = request.getParameter("key");
+        String format = null;
+        String command = null;
+        String type = null;
+
+        try {
+            ApiUser user = null;
+            if(key == null)
+                user = control.getUser(Resource.get("user.default"));
+            else
+                user = control.getUser(key);
+            if(user == null)
+                throw new ApiAuthenticationException();
+
+            format = stok.nextToken();
+            command = stok.nextToken();
+            type = stok.nextToken();
+
+            ApiMethod method = null;
+            if((method = methods.get(command)) != null ) {
+                if(!method.validFormat(format))
+                    throw new ApiFormatException(format);
+
+                if(!method.validType(type))
+                    throw new ApiTypeException(type);
+
+                AbstractApiExecute executionClass = method.getInstanceOfExecutionClass();
+                Object obj = executionClass.execute(request, response, getMore(format, type, stok));
+
+                if(obj == null) {
+                    throw new ApiInternalException();
+                }
+                else {
+                    if(format.equals("xml")) {
+                        out.print(executionClass.toXml(obj, method.getXstreamClasses()));
+                    }
+                    else if(format.equals("json")) {
+                        out.print(executionClass.toJson(obj));
+                    }
+                    else {
+                        out.print(executionClass.toOther(obj, format));
+                    }
+                }
+
+
+                if(method.getWriteMetric()) {
+                    logger.info("writing metric");
+
+                    control.addMetric(user.getId(), constructUrl(uri, request), request.getRemoteAddr());
+                }
+            }
+            else {
+                throw new ApiCommandException(command);
+            }
+        }
+        catch (ApiCommandException ace) {
+            out.write(getError("error", "Invalid command: " + ace.getMessage()
+                    + ", please view API documentation.", format));
+            logger.warn(ace);
+        }
+        catch (ApiFormatException afe) {
+            out.write(getError("error", "Invalid format: " + afe.getMessage()  +
+                    " for command: " + command + ", please review API documentation.", format));
+            logger.warn(afe);
+        }
+        catch (ApiTypeException ate) {
+            out.write(getError("error", "Invalid input " + ate.getMessage()
+                    + " for command: " + command +", please review API documentation.", format));
+            logger.warn(ate);
+        }
+        catch (ApiAuthenticationException aae) {
+            out.write(getError("error", "Could not be authorized.", format));
+            logger.warn(aae);
+        }
+        catch (Exception e) {
+            out.write(getError("error", "Invalid request " + constructUrl(uri, request)
+                    + ", please check that your input is properly formatted " +
+                            "and review the API documentation.", format));
+            logger.warn(e);
+        }
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        doGet(request, response);
+    }
+
+    public String getError(String name, String reason, String format) {
+        logger.warn("Error! name: " + name + ", reason: " + reason);
+
+        ErrorResponse e = new ErrorResponse(reason);
+        if(format != null && format.matches("xml|kml")) {
+            XStream xstream = new XStream(new DomDriver());
+            xstream.processAnnotations(new Class[]{ValidateResponse.class, ErrorResponse.class});
+            return xstream.toXML(e);
+        }
+        else if(format != null && format.matches("json")) {
+            Gson gson = new Gson();
+            return gson.toJson(e);
+        }
+        else {
+            return "ERROR," + reason.replaceAll(",", "");
+        }
+
+    }
+
+    public String constructUrl(String uri, HttpServletRequest req) {
+        String ret = uri + "?";
+        Enumeration<?> params = req.getParameterNames();
+        while(params.hasMoreElements()) {
+            String elem = (String)params.nextElement();
+            ret += elem + "=" + req.getParameter(elem) + "&";
+        }
+        return ret.replaceAll("&$", "");
+    }
+
+    public ArrayList<String> getMore(String format, String type, StringTokenizer stok) {
+        ArrayList<String> strings = new ArrayList<String>();
+        strings.add(format);
+        strings.add(type);
+        while(stok.hasMoreTokens()) {
+            strings.add(stok.nextToken());
+        }
+        return strings;
+    }
+
+
 }
--- src/main/java/gov/nysenate/sage/servlets/UploadServlet.java
+++ src/main/java/gov/nysenate/sage/servlets/UploadServlet.java
@@ -4,7 +4,6 @@ import gov.nysenate.sage.BulkProcessing.Processor;
 import gov.nysenate.sage.model.BulkProcessing.BulkFileType;
 import gov.nysenate.sage.model.BulkProcessing.JobProcess;
 import gov.nysenate.sage.util.Connect;
-import gov.nysenate.sage.util.Resource;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
@@ -29,176 +28,178 @@ import org.apache.log4j.Logger;
  * Servlet implementation class UploadServlet
  */
 public class UploadServlet extends HttpServlet {
-	private Logger logger = Logger.getLogger(UploadServlet.class);
-	
-	private static final long serialVersionUID = 1L;
-	
-	private static String realPath;
-	private static final String sep = System.getProperty("file.separator");
-	
+    private final Logger logger = Logger.getLogger(UploadServlet.class);
+
+    private static final long serialVersionUID = 1L;
+
+    private static String realPath;
+    private static final String sep = System.getProperty("file.separator");
+
     public UploadServlet() {
         super();
     }
-    
+
+    @Override
     public void init(ServletConfig config) throws ServletException {
-    	super.init(config);
-    	Resource.init(this);
-    	
-    	realPath = getServletContext().getRealPath(sep).replaceAll(sep + "GeoApi.*$", "") 
-			+ sep
-    		+ "upload" 
-			+ sep;
+        super.init(config);
+
+        realPath = getServletContext().getRealPath(sep).replaceAll(sep + "GeoApi.*$", "")
+            + sep
+            + "upload"
+            + sep;
     }
 
 
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		request.getRequestDispatcher("/upload.jsp").forward(request, response);
-	}
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.getRequestDispatcher("/upload.jsp").forward(request, response);
+    }
 
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		HttpSession session = request.getSession();
-		PrintWriter writer = response.getWriter();
-		
-		String uri = URLDecoder.decode(request.getRequestURI(), "utf-8");
-		
-		String command = uri.replaceAll("/?(GeoApi)?/?upload/?","");
-		
-		String oldFilePath = (String) session.getAttribute("currentFilePath");
-		File oldFile = null;
-		
-		if(oldFilePath != null)
-			oldFile = new File(oldFilePath);
-				
-		if(command.equals("")) {
-			logger.info("Processing form submission");
-			
-			String email = request.getParameter("email");
-			String fileName = request.getParameter("fileName");
-			String header = request.getParameter("header");
-			
-			try {
-				if(oldFile == null || !oldFile.exists())
-					throw new SubmitException();
-				else {
-					BulkFileType bulkFileType = (BulkFileType) session.getAttribute("bulkFileType");
-					
-					if(bulkFileType.header().equals(header) &&
-							oldFilePath.matches(".*" + fileName + "$")) {
-						
-						JobProcess jp = new JobProcess(email, fileName, bulkFileType.clazz().getName());
-						
-						Connect connect = new Connect();
-						connect.persist(jp);
-						connect.close();
-						
-						session.removeAttribute("bulkFileType");
-						session.removeAttribute("currentFilePath");
-						
-						session.setAttribute("email", email);
-						response.sendRedirect("/GeoApi/upload/message");
-					}
-					else
-						throw new SubmitException();
-				}
-			}
-			catch (SubmitException se) {
-				request.setAttribute("email", email);
-				request.setAttribute("error","There was an internal issue, please try again.");
-				request.getRequestDispatcher("/upload.jsp").forward(request, response);
-			}
-		}
-		else if(command.equals("file")) {
-			logger.info("Processing file upload");
-			
-			doUpload(session, request, response, writer, oldFile);
-		}
-	}
-	
-	public void doUpload(HttpSession session, HttpServletRequest request, 
-			HttpServletResponse response, PrintWriter writer, File oldFile) throws IOException {
-				
-		BufferedReader br = null;
-		BufferedWriter bw = null;
-		
-		String fileName = request.getHeader("X-File-Name").replaceAll("( |%20)","_");
-		String filePath = realPath + (new Date().getTime()) + "-" + fileName;
-		
-		logger.info("Creating file: " + filePath);
-		
-		File file = new File(filePath);
-		try {
-			br = new BufferedReader(new InputStreamReader(request.getInputStream()));
-			
-			String newLineDelim = Processor.getNewLineDelim(br);
-			
-			bw = new BufferedWriter(new FileWriter(file));
-			int count = 0;
-						
-			String header = br.readLine();
-			bw.write(header + newLineDelim);
-			
-			//Check BulkFileType enum to see if header is ok for processing
-			BulkFileType bulkFileType = this.getBulkFileType(header);
-			
-			if(bulkFileType == null)
-				writer.print("{success:false,type:\"null\"}");
-			else {
-				logger.info("Request type: " + bulkFileType.clazz().getSimpleName());
-				
-				String in = null;
-				while((in = br.readLine()) != null) {
-					bw.write(in + newLineDelim);
-					
-					count++;
-				}
-				
-				session.setAttribute("currentFilePath", filePath);
-				session.setAttribute("bulkFileType", bulkFileType);
-				response.setStatus(HttpServletResponse.SC_OK);
-				writer.print("{success:true,file:\"" + fileName + "\",count:" + count + 
-						",systemFile:\"" + file.getName() + "\",header:\"" + header + 
-						"\",type:\"" + bulkFileType.type() + "\"}");
-			}
-		}
-		catch (IOException e) {
-			response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
-			logger.error(e);
-			writer.print("{success:false}");
-			
-			if(file.exists()) {
-				file.delete();
-			}
-		}
-		finally {
-			br.close();
-			bw.close();
-			
-			if(oldFile != null && oldFile.exists())
-				oldFile.delete();
-		}
-		
-		writer.flush();
-		writer.close();
-	}
-	
-	public BulkFileType getBulkFileType(String header) {
-		for(BulkFileType bulkFileType:BulkFileType.values()) {
-			if(bulkFileType.header().equals(header)) {
-				return bulkFileType;
-			}
-		}
-		return null;
-	}
-	
-	class SubmitException extends Exception {
-		private static final long serialVersionUID = 1L;
-		
-		public SubmitException() {
-			super();
-		}
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        HttpSession session = request.getSession();
+        PrintWriter writer = response.getWriter();
 
-		public SubmitException(String message) {
-			super(message);
-		}
-	}
+        String uri = URLDecoder.decode(request.getRequestURI(), "utf-8");
+
+        String command = uri.replaceAll("/?(GeoApi)?/?upload/?","");
+
+        String oldFilePath = (String) session.getAttribute("currentFilePath");
+        File oldFile = null;
+
+        if(oldFilePath != null)
+            oldFile = new File(oldFilePath);
+
+        if(command.equals("")) {
+            logger.info("Processing form submission");
+
+            String email = request.getParameter("email");
+            String fileName = request.getParameter("fileName");
+            String header = request.getParameter("header");
+
+            try {
+                if(oldFile == null || !oldFile.exists())
+                    throw new SubmitException();
+                else {
+                    BulkFileType bulkFileType = (BulkFileType) session.getAttribute("bulkFileType");
+
+                    if(bulkFileType.header().equals(header) &&
+                            oldFilePath.matches(".*" + fileName + "$")) {
+
+                        JobProcess jp = new JobProcess(email, fileName, bulkFileType.clazz().getName());
+
+                        Connect connect = new Connect();
+                        connect.persist(jp);
+                        connect.close();
+
+                        session.removeAttribute("bulkFileType");
+                        session.removeAttribute("currentFilePath");
+
+                        session.setAttribute("email", email);
+                        response.sendRedirect("/GeoApi/upload/message");
+                    }
+                    else
+                        throw new SubmitException();
+                }
+            }
+            catch (SubmitException se) {
+                request.setAttribute("email", email);
+                request.setAttribute("error","There was an internal issue, please try again.");
+                request.getRequestDispatcher("/upload.jsp").forward(request, response);
+            }
+        }
+        else if(command.equals("file")) {
+            logger.info("Processing file upload");
+
+            doUpload(session, request, response, writer, oldFile);
+        }
+    }
+
+    public void doUpload(HttpSession session, HttpServletRequest request,
+            HttpServletResponse response, PrintWriter writer, File oldFile) throws IOException {
+
+        BufferedReader br = null;
+        BufferedWriter bw = null;
+
+        String fileName = request.getHeader("X-File-Name").replaceAll("( |%20)","_");
+        String filePath = realPath + (new Date().getTime()) + "-" + fileName;
+
+        logger.info("Creating file: " + filePath);
+
+        File file = new File(filePath);
+        try {
+            br = new BufferedReader(new InputStreamReader(request.getInputStream()));
+
+            String newLineDelim = Processor.getNewLineDelim(br);
+
+            bw = new BufferedWriter(new FileWriter(file));
+            int count = 0;
+
+            String header = br.readLine();
+            bw.write(header + newLineDelim);
+
+            //Check BulkFileType enum to see if header is ok for processing
+            BulkFileType bulkFileType = this.getBulkFileType(header);
+
+            if(bulkFileType == null)
+                writer.print("{success:false,type:\"null\"}");
+            else {
+                logger.info("Request type: " + bulkFileType.clazz().getSimpleName());
+
+                String in = null;
+                while((in = br.readLine()) != null) {
+                    bw.write(in + newLineDelim);
+
+                    count++;
+                }
+
+                session.setAttribute("currentFilePath", filePath);
+                session.setAttribute("bulkFileType", bulkFileType);
+                response.setStatus(HttpServletResponse.SC_OK);
+                writer.print("{success:true,file:\"" + fileName + "\",count:" + count +
+                        ",systemFile:\"" + file.getName() + "\",header:\"" + header +
+                        "\",type:\"" + bulkFileType.type() + "\"}");
+            }
+        }
+        catch (IOException e) {
+            response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+            logger.error(e);
+            writer.print("{success:false}");
+
+            if(file.exists()) {
+                file.delete();
+            }
+        }
+        finally {
+            br.close();
+            bw.close();
+
+            if(oldFile != null && oldFile.exists())
+                oldFile.delete();
+        }
+
+        writer.flush();
+        writer.close();
+    }
+
+    public BulkFileType getBulkFileType(String header) {
+        for(BulkFileType bulkFileType:BulkFileType.values()) {
+            if(bulkFileType.header().equals(header)) {
+                return bulkFileType;
+            }
+        }
+        return null;
+    }
+
+    class SubmitException extends Exception {
+        private static final long serialVersionUID = 1L;
+
+        public SubmitException() {
+            super();
+        }
+
+        public SubmitException(String message) {
+            super(message);
+        }
+    }
 }
