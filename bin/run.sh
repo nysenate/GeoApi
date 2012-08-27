8b0b96ffa76ee29b2a52a2625a75ce4bf2c00462
Graylin Kim <graylin.kim@gmail.com>
2012-08-27 13:51:44 -0400

    Add bash tools for running Java scripts.

---
+++ bin/run.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+source $(dirname "$0")/utils.sh
+
+BASE="$ROOTDIR/target/GeoApi-$VERSION/WEB-INF"
+
+case $1 in
+    --help | -h | help) echo "USAGE: `basename $0` SCRIPT_CLASS_NAME|help <args>"; exit;;
+esac
+
+# TODO: This memory size should be an adjustable parameter
+java -Xmx1024m -Xms16m -cp $BASE/classes/:$BASE/lib/* gov.nysenate.sage.scripts.$1 $@
+
