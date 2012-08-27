f9c5bac32832df4c3f13f3bc8e37175d6064a705
Graylin Kim <graylin.kim@gmail.com>
2012-08-27 00:30:08 -0400

    Add district models for school and town districts.

---
+++ src/main/java/gov/nysenate/sage/model/districts/Town.java
@@ -0,0 +1,21 @@
+package gov.nysenate.sage.model.districts;
+
+public class Town {
+    String district;
+
+    public Town() {
+
+    }
+
+    public Town(String district) {
+        this.district = district;
+    }
+
+    public String getDistrict() {
+        return district;
+    }
+
+    public void setDistrict(String district) {
+        this.district = district;
+    }
+}
