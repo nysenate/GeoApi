041fe7224a2708bfffaf80854d635f8c3226f57e
Graylin Kim <graylin.kim@gmail.com>
2012-11-28 12:41:33 -0500

    Add threading and fall back to shapefiles.
    Use 3 threads and rubygeocoder when falling back to shape files.

--- src/main/java/gov/nysenate/sage/api/methods/BulkDistrictMethod.java
+++ src/main/java/gov/nysenate/sage/api/methods/BulkDistrictMethod.java
@@ -1,5 +1,7 @@
 package gov.nysenate.sage.api.methods;
 
+import gov.nysenate.sage.Address;
+import gov.nysenate.sage.Result;
 import gov.nysenate.sage.api.exceptions.ApiInternalException;
 import gov.nysenate.sage.api.exceptions.ApiTypeException;
 import gov.nysenate.sage.boe.AddressUtils;
@@ -8,6 +10,9 @@ import gov.nysenate.sage.boe.BOEStreetAddress;
 import gov.nysenate.sage.boe.DistrictLookup;
 import gov.nysenate.sage.model.ApiExecution;
 import gov.nysenate.sage.service.DistrictService;
+import gov.nysenate.sage.service.DistrictService.DistException;
+import gov.nysenate.sage.service.GeoService;
+import gov.nysenate.sage.service.GeoService.GeoException;
 import gov.nysenate.sage.util.Resource;
 
 import java.io.IOException;
@@ -16,6 +21,11 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Iterator;
 import java.util.List;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -27,14 +37,21 @@ import org.json.JSONObject;
 import com.mysql.jdbc.jdbc2.optional.MysqlDataSource;
 
 public class BulkDistrictMethod extends ApiExecution {
+
     public static final ArrayList<DistrictService.TYPE> districtTypes = new ArrayList<DistrictService.TYPE>(Arrays.asList(DistrictService.TYPE.ASSEMBLY,DistrictService.TYPE.CONGRESSIONAL,DistrictService.TYPE.SENATE,DistrictService.TYPE.COUNTY,DistrictService.TYPE.ELECTION));
 
     private class BluebirdAddress extends BOEStreetAddress {
         public String id;
+        public double latitude;
+        public double longitude;
+        public int geo_accuracy;
+
         public BluebirdAddress(String id) { this.id = id; }
     }
 
-    public final DistrictLookup streetData;
+    private final GeoService geoService;
+    private final DistrictLookup streetData;
+    private final DistrictService districtService;
 
     public BulkDistrictMethod() throws Exception {
         Resource config = new Resource();
@@ -45,7 +62,9 @@ public class BulkDistrictMethod extends ApiExecution {
         db.setPassword(config.fetch("db.pass"));
         db.setDatabaseName(config.fetch("db.name"));
 
-        streetData =  new DistrictLookup(db);
+        streetData = new DistrictLookup(db);
+        geoService = new GeoService();
+        districtService = new DistrictService();
     }
 
     public static void main(String[] args) throws Exception {
@@ -122,37 +141,156 @@ public class BulkDistrictMethod extends ApiExecution {
     }
 
     @SuppressWarnings("unused")
-    private static class Result {
+    private static class BulkResult {
         public static enum STATUS { MATCH, INVALID, NOMATCH, MULTIMATCH };
 
         public STATUS status_code;
         public String message;
         public String address_id;
-        public int countyCode;
-        public int assemblyCode;
-        public int congressionalCode;
-        public int senateCode;
-        public int electionCode;
+        public int county_code;
+        public int assembly_code;
+        public int congressional_code;
+        public int senate_code;
+        public int election_code;
         public String address;
+        public double latitude;
+        public double longitude;
+        public int geo_accuracy;
 
-        public Result(STATUS status, String message, BluebirdAddress address, BOEAddressRange match) {
+        public BulkResult(STATUS status, String message, BluebirdAddress address, BOEAddressRange match) {
             this.address_id = address.id;
             this.status_code = status;
             this.message = message;
             this.address = address.toString();
-            this.electionCode = match.electionCode;
-            this.senateCode = match.senateCode;
-            this.congressionalCode = match.congressionalCode;
-            this.assemblyCode = match.assemblyCode;
-            this.countyCode = match.countyCode;
+            this.election_code = match.electionCode;
+            this.senate_code = match.senateCode;
+            this.congressional_code = match.congressionalCode;
+            this.assembly_code = match.assemblyCode;
+            this.county_code = match.countyCode;
+            this.latitude = address.latitude;
+            this.longitude = address.longitude;
+            this.geo_accuracy = address.geo_accuracy;
         }
     }
 
+    private BOEAddressRange SAGE2Bluebird(Address address) {
+        if (address == null) {
+            return null;
+        }
+
+        BOEAddressRange range = new BOEAddressRange();
+        range.street = address.addr2;
+        range.town = address.city;
+        range.state = address.state;
+        try {
+            range.zip5 = Integer.parseInt(address.zip5);
+        } catch (NumberFormatException e) {
+            range.zip5 = 0;
+        }
+        range.assemblyCode = address.assembly_code;
+        range.congressionalCode = address.congressional_code;
+        range.senateCode = address.senate_code;
+        range.countyCode = address.county_code;
+        return range;
+    }
+    private Address Bluebird2SAGE(BluebirdAddress address) {
+        if (address == null) {
+            return null;
+        }
+
+        return new Address(
+            address.street,
+            address.town,
+            address.state,
+            (address.zip5 != 0) ? String.valueOf(address.zip5) : ""
+        );
+    }
+
+    private BulkResult shapefileLookup(BluebirdAddress bluebirdAddress) throws GeoException, DistException{
+        if (bluebirdAddress == null) {
+            return null;
+        }
+
+        Address address = Bluebird2SAGE(bluebirdAddress);
+        Result geoResult = geoService.geocode(address, "rubygeocoder");
+        if (!geoResult.status_code.equals("0")) {
+            throw new GeoException("Bad status code "+geoResult.status_code);
+        }
+        if (geoResult.addresses.size()==0) {
+            System.out.println(address);
+            System.out.println(geoResult.status_code+" - "+geoResult.messages);
+        }
+        Result distResult = districtService.assignDistricts(geoResult.addresses.get(0), Arrays.asList(DistrictService.TYPE.ASSEMBLY,DistrictService.TYPE.CONGRESSIONAL,DistrictService.TYPE.SENATE,DistrictService.TYPE.COUNTY), "geoserver");
+        // Result distResult = districtService.assignDistricts(geoResult.addresses.get(0), Arrays.asList(DistrictService.TYPE.SENATE), "geoserver");
+        if (!distResult.status_code.equals("0")) {
+            throw new DistException("Bad status code "+distResult.status_code);
+        }
+
+        Address resultAddress = distResult.address;
+        bluebirdAddress.latitude = resultAddress.latitude;
+        bluebirdAddress.longitude = resultAddress.longitude;
+        bluebirdAddress.geo_accuracy = resultAddress.geocode_quality;
+        BOEAddressRange addressRange = SAGE2Bluebird(distResult.address);
+        return new BulkResult(BulkResult.STATUS.MATCH, "SHAPEFILE MATCH for"+resultAddress, bluebirdAddress, addressRange );
+    }
+
+    public class ParallelRequest implements Callable<BulkResult> {
+
+        private final BluebirdAddress address;
+        public ParallelRequest(BluebirdAddress address) {
+            this.address = address;
+        }
+
+        @Override
+        public BulkResult call() throws SQLException {
+            if (address!=null) {
+                List<BOEAddressRange> matches = streetData.getRanges(address,true);
+                try {
+                    if (matches.size()==1) {
+                        return new BulkResult(BulkResult.STATUS.MATCH,"EXACT MATCH",address,matches.get(0));
+
+                    } else if (matches.size()==0) {
+                        // If at first you don't succeed, try again without the building number
+                        matches = streetData.getRanges(address,false);
+
+                        if (matches.size()==0) {
+                            return shapefileLookup(address);
+
+                        } else {
+                            // Consolidate these results to "range fill"
+                            BOEAddressRange consolidated = AddressUtils.consolidateRanges(matches);
+                            if (consolidated != null) {
+                                return new BulkResult(BulkResult.STATUS.MATCH,"CONSOLIDATED RANGEFILL",address, consolidated);
+                            } else {
+                                return shapefileLookup(address);
+                            }
+                        }
+
+                    } else {
+                        BOEAddressRange consolidated = AddressUtils.consolidateRanges(matches);
+                        if (consolidated != null) {
+                            return new BulkResult(BulkResult.STATUS.MATCH,"CONSOLIDATED MULTIMATCH",address, consolidated);
+                        } else {
+                            return shapefileLookup(address);
+                        }
+                    }
+                } catch (GeoException e) {
+                    e.printStackTrace();
+                    return new BulkResult(BulkResult.STATUS.NOMATCH, "GeoException for: "+address.toString(), address, new BOEAddressRange() );
+                } catch (DistException e) {
+                    e.printStackTrace();
+                    return new BulkResult(BulkResult.STATUS.NOMATCH, "DistException for: "+address.toString(), address, new BOEAddressRange() );
+                }
+
+            } else {
+                return new BulkResult(BulkResult.STATUS.INVALID,"Invalid JSON Entry",new BluebirdAddress("-1"),new BOEAddressRange());
+            }
+        }
+    }
     @Override
     public Object execute(HttpServletRequest request, HttpServletResponse response, ArrayList<String> more) throws ApiTypeException, ApiInternalException {
         String type = more.get(RequestCodes.TYPE.code());
-        ArrayList<Result> results = new ArrayList<Result>();
-        ArrayList<BOEAddressRange> noMatches = new ArrayList<BOEAddressRange>();
+        ArrayList<BulkResult> results = new ArrayList<BulkResult>();
         ArrayList<BluebirdAddress> bluebirdAddresses;
         try {
             if(type.equals("url")) {
@@ -164,51 +302,31 @@ public class BulkDistrictMethod extends ApiExecution {
                 throw new ApiTypeException(type);
             }
 
+            ExecutorService executor = Executors.newFixedThreadPool(3);
+            ArrayList<Future<BulkResult>> futureResults = new ArrayList<Future<BulkResult>>();
+
             for(BluebirdAddress address : bluebirdAddresses) {
-                if (address!=null) {
-                    List<BOEAddressRange> matches = streetData.getRanges(address,true);
+                futureResults.add(executor.submit(new ParallelRequest(address)));
+            }
 
-                    if (matches.size()==1) {
-                        results.add(new Result(Result.STATUS.MATCH,"EXACT MATCH",address,matches.get(0)));
-                        continue;
-
-                    } else if (matches.size()==0) {
-                        // If at first you don't succeed, try again without the building number
-                        matches = streetData.getRanges(address,false);
-
-                        if (matches.size()==0) {
-                            results.add(new Result(Result.STATUS.NOMATCH,"Street + Zip not Found for: "+address.toString(),address, new BOEAddressRange()));
-                        } else {
-                            // Consolidate these results to "range fill"
-                            BOEAddressRange consolidated = AddressUtils.consolidateRanges(matches);
-                            if (consolidated != null) {
-                                results.add(new Result(Result.STATUS.MATCH,"CONSOLIDATED RANGEFILL",address, consolidated));
-                            } else {
-                                results.add(new Result(Result.STATUS.NOMATCH, "RANGEFILL failed for: "+address.toString(), address, new BOEAddressRange()));
-                            }
-                        }
-                    } else {
-                        BOEAddressRange consolidated = AddressUtils.consolidateRanges(matches);
-                        if (consolidated != null) {
-                            results.add(new Result(Result.STATUS.MATCH,"CONSOLIDATED MULTIMATCH",address, consolidated));
-                        } else {
-                            results.add(new Result(Result.STATUS.MULTIMATCH, matches.size()+" inconsistent matches found for: "+address.toString(), address, new BOEAddressRange()));
-                        }
-                    }
-
-                } else {
-                    results.add(new Result(Result.STATUS.INVALID,"Invalid JSON Entry",new BluebirdAddress("-1"),new BOEAddressRange()));
+            for (Future<BulkResult> result : futureResults) {
+                try {
+                    results.add(result.get());
+                } catch (InterruptedException e) {
+                    throw new ApiInternalException(e);
+                } catch (ExecutionException e) {
+                    throw new ApiInternalException(e.getCause());
                 }
             }
+
+            executor.shutdown();
+
         } catch (IOException e) {
             e.printStackTrace();
             throw new ApiTypeException("No request body found.", e);
         } catch (JSONException e) {
             e.printStackTrace();
             throw new ApiTypeException("Invalid JSON recieved.",e);
-        } catch (SQLException e) {
-            e.printStackTrace();
-            throw new ApiInternalException("Internal DB Error", e);
         }
 
         return results;
