d735637de525a197cb80396d175c716cff51e3e1
Ash Islam <ashislam858@gmail.com>
2013-01-11 15:28:41 -0500

    function name changes

--- src/main/java/gov/nysenate/sage/util/JsonUtil.java
+++ src/main/java/gov/nysenate/sage/util/JsonUtil.java
@@ -3,9 +3,13 @@ package gov.nysenate.sage.util;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+/**
+  * Utility class for accessing JSONObject
+*/
+
 public class JsonUtil {
 
-    public static String jsonGetString(JSONObject json, String key) {
+    public static String getString(JSONObject json, String key) {
         try {
             return json.has(key) && !json.isNull(key) ? json.getString(key) : "";
         } catch (JSONException e) {
@@ -13,7 +17,7 @@ public class JsonUtil {
         }
     }
 
-    public static Double jsonGetDouble(JSONObject json, String key) {
+    public static Double getDouble(JSONObject json, String key) {
         try {
             return json.has(key) && !json.isNull(key) && !json.getString(key).equals("") ? json.getDouble(key) : 0;
         } catch (JSONException e) {
@@ -21,12 +25,11 @@ public class JsonUtil {
         }
     }
 
-    public static Integer jsonGetInteger(JSONObject json, String key) {
+    public static Integer getInteger(JSONObject json, String key) {
         try {
             return json.has(key) && !json.isNull(key) && !json.getString(key).equals("") ? json.getInt(key) : 0;
         } catch (JSONException e) {
             return null;
         }
     }
-
 }
