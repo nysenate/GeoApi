d3ad96359d7fc24ed23a7a093c3965f3f9bb2799
Ken Zalewski <zalewski.senate@gmail.com>
2013-01-17 14:36:20 -0500

    Reformatted code.

--- src/main/java/gov/nysenate/sage/util/ApiController.java
+++ src/main/java/gov/nysenate/sage/util/ApiController.java
@@ -20,118 +20,132 @@ import org.jasypt.util.password.BasicPasswordEncryptor;
 
 import com.google.gson.Gson;
 
-public class ApiController {
-	private final GeoServer geoserver;
 
-	public ApiController() throws Exception {
-	    geoserver = new GeoServer();
-	}
+public class ApiController
+{
+    private final GeoServer geoserver;
 
-	public static void main(String[] args) throws Exception {
-	    Connect db = new Connect();
+    public ApiController() throws Exception
+    {
+        geoserver = new GeoServer();
+    }
 
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		String in = "";
 
-		System.out.print("> ");
-		while(!(in = br.readLine()).equals("exit")) {
-			if(in.equals("create all")) {
-				System.out.println("indexing assembly... ");
-				new AssemblyScraper().index();
-				System.out.println("indexing congress... ");
-				new CongressScraper().index();
-				System.out.print("indexing senate... ");
-				new NYSenateServices().index();
-				System.out.println();
-			}
-			else if(in.equals("create sen")) {
-				System.out.print("indexing senate... ");
-				new NYSenateServices().index();
-				System.out.println();
-			}
-			else if(in.equals("create ass")) {
-				System.out.println("indexing assembly... ");
-				new AssemblyScraper().index();
-			}
-			else if(in.equals("create con")) {
-				System.out.println("indexing congress... ");
-				new CongressScraper().index();
-			}
-			else if(in.startsWith("add user")) {
-				Pattern p = Pattern.compile("add user \"(.+?)\" \"(.+?)\" \"(.+?)\"");
-				Matcher m = p.matcher(in);
-				if(m.find()) {
-					System.out.println("key for user is... " + new ApiController().addUser(m.group(3), m.group(1), m.group(2), db));
-				}
-				else {
-					System.out.println("proper format is: add user \"<name>\" \"<description>\" \"<key>\"");
-				}
-			}
-			System.out.print("> ");
-		}
-		db.close();
-	}
+    public static void main(String[] args) throws Exception
+    {
+        Connect db = new Connect();
 
-    public String addUser(String apiKey, String name, String description, Connect db) {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String in = "";
+
+        System.out.print("> ");
+ 
+        while (!(in = br.readLine()).equals("exit")) {
+            if (in.equals("create all")) {
+                System.out.println("indexing assembly... ");
+                new AssemblyScraper().index();
+                System.out.println("indexing congress... ");
+                new CongressScraper().index();
+                System.out.print("indexing senate... ");
+                new NYSenateServices().index();
+                System.out.println();
+            }
+            else if (in.equals("create sen")) {
+                System.out.print("indexing senate... ");
+                new NYSenateServices().index();
+                System.out.println();
+            }
+            else if (in.equals("create ass")) {
+                System.out.println("indexing assembly... ");
+                new AssemblyScraper().index();
+            }
+            else if (in.equals("create con")) {
+                System.out.println("indexing congress... ");
+                new CongressScraper().index();
+            }
+            else if (in.startsWith("add user")) {
+                Pattern p = Pattern.compile("add user \"(.+?)\" \"(.+?)\" \"(.+?)\"");
+                Matcher m = p.matcher(in);
+                if(m.find()) {
+                    System.out.println("key for user is... " + new ApiController().addUser(m.group(3), m.group(1), m.group(2), db));
+                }
+                else {
+                    System.out.println("proper format is: add user \"<name>\" \"<description>\" \"<key>\"");
+                }
+            }
+            System.out.print("> ");
+        }
+        db.close();
+    }
+
+
+    public String addUser(String apiKey, String name, String description, Connect db)
+    {
         BasicPasswordEncryptor pe = new BasicPasswordEncryptor();
         String ep = pe.encryptPassword(apiKey).replaceAll("\\W", "");
         db.persist(new ApiUser(ep, name, description));
         return ep;
     }
 
-    public ApiUser getUser(String apiKey, Connect db) {
+
+    public ApiUser getUser(String apiKey, Connect db)
+    {
         ApiUser user = null;
         try {
-            user = (ApiUser) db.getObject(ApiUser.class, "apikey",apiKey);
+            user = (ApiUser) db.getObject(ApiUser.class, "apikey", apiKey);
         } catch (Exception e) {
             e.printStackTrace();
         }
         return user;
     }
 
-    public static ApiUser getUser(String apiKey) throws Exception {
+
+    public static ApiUser getUser(String apiKey) throws Exception
+    {
         Connect db = new Connect();
         try {
             return (ApiUser)db.getObject(ApiUser.class, "apikey", apiKey);
-        } finally {
+        }
+        finally {
             db.close();
         }
     }
 
-	/*
-	 * used to write the json used for maps and for raw data
-	 */
-	public void writeJson(File writeDirectory, File zoomFile, boolean geo) throws Exception {
-		Connect c = new Connect();
-		Gson gson = new Gson();
 
-		FileUtils.forceMkdir(writeDirectory);
+    /*
+     * used to write the json used for maps and for raw data
+     */
+    public void writeJson(File writeDirectory, File zoomFile, boolean geo) throws Exception
+    {
+        Connect c = new Connect();
+        Gson gson = new Gson();
 
-		for(int i = 1; i <= 63; i++) {
-			FileWriter fw = new FileWriter(new File(writeDirectory,"/sd" + i + ".json"));
+        FileUtils.forceMkdir(writeDirectory);
 
-			PrintWriter pw = new PrintWriter(fw);
+        for (int i = 1; i <= 63; i++) {
+            FileWriter fw = new FileWriter(new File(writeDirectory,"/sd" + i + ".json"));
 
-			Result result = geoserver.getFeatures("DISTRICT="+i, DistrictService.TYPE.SENATE);
-			if (!result.status_code.equals("0")) {
-			    System.out.println("ERROR: "+result.messages.get(0));
-			    continue;
-			}
+            PrintWriter pw = new PrintWriter(fw);
 
-			SenateMapInfo smi;
-			Senate senate = (Senate) c.getObject(Senate.class, "district", "State Senate District " + i);
+            Result result = geoserver.getFeatures("DISTRICT="+i, DistrictService.TYPE.SENATE);
+            if (!result.status_code.equals("0")) {
+                System.out.println("ERROR: "+result.messages.get(0));
+                continue;
+            }
 
-			smi = new SenateMapInfo(
-			        result.address.latitude,
-                    result.address.longitude,
-					0,null,null,senate);
+            SenateMapInfo smi;
+            Senate senate = (Senate) c.getObject(Senate.class, "district", "State Senate District " + i);
 
-            if(geo)
+            smi = new SenateMapInfo(result.address.latitude,
+                                    result.address.longitude,
+                                    0, null, null, senate);
+
+            if (geo)
                 pw.write(gson.toJson(smi));
             else
                 pw.write(gson.toJson(senate));
 
-			pw.close();
-		}
-	}
+            pw.close();
+        }
+    }
 }
