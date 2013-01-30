5391f1560ad711e123a11a7b708bf321f1660ca0
Ash Islam <ashislam858@gmail.com>
2013-01-30 12:56:35 -0500

    typo on senate school

--- src/main/java/gov/nysenate/sage/boe/DistrictLookup.java
+++ src/main/java/gov/nysenate/sage/boe/DistrictLookup.java
@@ -42,7 +42,7 @@ public class DistrictLookup {
         column_map.put("ward_code", "wardCode");
         column_map.put("cc_code", "ccCode");
         column_map.put("school_code", "schoolCode");
-        column_map.put("senate_town_code", "senateSchoolCode");
+        column_map.put("senate_school_code", "senateSchoolCode");
         column_map.put("cleg_code","clegCode");
         column_map.put("fire_code","fireCode");
         column_map.put("city_code","cityCode");
