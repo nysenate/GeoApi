619d969161dfdf9316f10a04d9cdd0369c1b12c8
Ken Zalewski <zalewski.senate@gmail.com>
2013-02-07 04:52:40 -0500

    Now using accessor methods on the Result object.

--- src/main/java/gov/nysenate/sage/api/methods/DistrictsMethod.java
+++ src/main/java/gov/nysenate/sage/api/methods/DistrictsMethod.java
@@ -27,144 +27,119 @@ import java.util.List;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-public class DistrictsMethod extends ApiExecution {
-    private final GeoService geoService;
-    private final DistrictService districtService;
 
-    public DistrictsMethod() throws Exception {
-        geoService = new GeoService();
-        districtService = new DistrictService();
+public class DistrictsMethod extends ApiExecution
+{
+  private final GeoService geoService;
+  private final DistrictService districtService;
+
+
+  public DistrictsMethod() throws Exception
+  {
+    geoService = new GeoService();
+    districtService = new DistrictService();
+  }
+
+
+  @Override
+  public Object execute(HttpServletRequest request, HttpServletResponse response, ArrayList<String> more) throws ApiFormatException, ApiTypeException, ApiInternalException
+  {
+    String type = more.get(RequestCodes.TYPE.code());
+    String service = request.getParameter("service");
+    if (service == null) {
+      service = "geocoder";
     }
 
-    public Object getDistricts(Address address, String service) throws ApiInternalException {
-        Connect db = new Connect();
-        try {
-            DistrictResponse dr = new DistrictResponse();
-            dr.validated = false;
-            dr.setAddress(address.as_raw());
+    Address address = null;
+    if (type.equals("addr")) {
+      if (service.equals("geocoder")) {
+        throw new ApiFormatException("Ruby Geocoder cannot accept an unparsed address");
+      }
+      address = new Address(more.get(RequestCodes.ADDRESS.code()));
+    }
+    else if (type.equals("extended")) {
+      address = new Address(
+        request.getParameter("addr1"),
+        request.getParameter("addr2"),
+        request.getParameter("city"),
+        request.getParameter("state"),
+        request.getParameter("zip5"),
+        request.getParameter("zip4")
+      );
+    }
+    else if (type.equals("latlon")) {
+      address = new Address();
+      String[] coord = more.get(RequestCodes.LATLON.code()).split(",");
+      address.setGeocode(Double.parseDouble(coord[0]), Double.parseDouble(coord[1]), 100);
+    }
+    else {
+      throw new ApiTypeException(type);
+    }
 
-            if (address.is_geocoded()) {
-                dr.geocoded = true;
-                dr.setLat(address.latitude);
-                dr.setLon(address.longitude);
+    return getDistricts(address, service);
+  } // execute()
 
-            } else {
-                Result result = geoService.geocode(address, service);
-                if (result == null || !result.status_code.equals("0")) {
-                    dr.geocoded = false;
-                    dr.errors.add(result.messages.get(0));
-                } else {
-                    dr.geocoded = true;
-                    address = result.addresses.get(0);
-                    dr.setLat(address.latitude);
-                    dr.setLon(address.longitude);
-                }
-            }
 
-            List<DistrictService.TYPE> types = Arrays.asList(DistrictService.TYPE.ASSEMBLY,DistrictService.TYPE.CONGRESSIONAL,DistrictService.TYPE.SENATE,DistrictService.TYPE.SCHOOL,DistrictService.TYPE.COUNTY,DistrictService.TYPE.TOWN);
-            Result result = districtService.assignDistricts(address, types, "geoserver");
-            if (result == null || !result.status_code.equals("0")) {
-                dr.distassigned = false;
-                dr.errors.addAll(result.messages);
+  public Object getDistricts(Address address, String service) throws ApiInternalException
+  {
+    Connect db = new Connect();
+    try {
+      DistrictResponse dr = new DistrictResponse();
+      dr.validated = false;
+      dr.setAddress(address.as_raw());
 
-            } else {
-                dr.setAssembly((Assembly)db.getObject(Assembly.class, "district", "Assembly District "+result.address.assembly_code));
-                dr.setCongressional((Congressional)db.getObject(Congressional.class, "district", "Congressional District "+result.address.congressional_code));
-                dr.setCounty(new County(result.address.county_name));
-                // dr.setElection(new Election("Election District "+result.address.election_code));
-                dr.setElection(new Election(""));
-                dr.setSenate((Senate)db.getObject(Senate.class, "district", "State Senate District "+result.address.senate_code));
-                dr.setSchool(new School("School District "+result.address.school_code));
-                dr.setTown(new Town("Town " + result.address.town_code));
-            }
+      if (address.is_geocoded()) {
+        dr.geocoded = true;
+        dr.setLat(address.latitude);
+        dr.setLon(address.longitude);
 
-            return dr;
-        } catch (UnsupportedEncodingException e) {
-            throw new ApiInternalException("UTF-8 unsupported uncoding.", e);
-        } catch (IOException e) {
-            throw new ApiInternalException("Unable to connect to database.", e);
-        } catch (Exception e) {
-            throw new ApiInternalException("Error retrieving object.", e);
-        } finally {
-            db.close();
+      }
+      else {
+        Result result = geoService.geocode(address, service);
+        if (result == null || !result.getStatus().equals("0")) {
+          dr.geocoded = false;
+          dr.errors.add(result.getFirstMessage());
         }
-    }
-
-    @Override
-    public Object execute(HttpServletRequest request, HttpServletResponse response, ArrayList<String> more) throws ApiFormatException, ApiTypeException, ApiInternalException {
-        String type = more.get(RequestCodes.TYPE.code());
-        String service = request.getParameter("service");
-        if (service == null) service = "geocoder";
-
-        Address address = null;
-        if (type.equals("addr")) {
-            if (service.equals("geocoder")) {
-                throw new ApiFormatException("Ruby Geocoder cannot accept an unparsed address");
-            }
-            address = new Address(more.get(RequestCodes.ADDRESS.code()));
-        } else if (type.equals("extended")) {
-            address = new Address(
-                request.getParameter("addr1"),
-                request.getParameter("addr2"),
-                request.getParameter("city"),
-                request.getParameter("state"),
-                request.getParameter("zip5"),
-                request.getParameter("zip4")
-            );
-        } else if (type.equals("latlon")) {
-            address = new Address();
-            String[] coord = more.get(RequestCodes.LATLON.code()).split(",");
-            address.setGeocode(Double.parseDouble(coord[0]), Double.parseDouble(coord[1]), 100);
-        } else {
-            throw new ApiTypeException(type);
+        else {
+          dr.geocoded = true;
+          address = result.getFirstAddress();
+          dr.setLat(address.latitude);
+          dr.setLon(address.longitude);
         }
+      }
 
-        return getDistricts(address, service);
+      List<DistrictService.TYPE> types = Arrays.asList(DistrictService.TYPE.ASSEMBLY,DistrictService.TYPE.CONGRESSIONAL,DistrictService.TYPE.SENATE,DistrictService.TYPE.SCHOOL,DistrictService.TYPE.COUNTY,DistrictService.TYPE.TOWN);
+      Result result = districtService.assignDistricts(address, types, "geoserver");
+      if (result == null || !result.getStatus().equals("0")) {
+        dr.distassigned = false;
+        dr.errors.addAll(result.getMessages());
+
+      }
+      else {
+        Address addr = result.getAddress();
+        dr.setAssembly((Assembly)db.getObject(Assembly.class, "district", "Assembly District "+addr.assembly_code));
+        dr.setCongressional((Congressional)db.getObject(Congressional.class, "district", "Congressional District "+addr.congressional_code));
+        dr.setCounty(new County(addr.county_name));
+        // dr.setElection(new Election("Election District "+addr.election_code));
+        dr.setElection(new Election(""));
+        dr.setSenate((Senate)db.getObject(Senate.class, "district", "State Senate District "+addr.senate_code));
+        dr.setSchool(new School("School District "+addr.school_code));
+        dr.setTown(new Town("Town " + addr.town_code));
+      }
+
+      return dr;
     }
-    /*
-	@Override
-	public Object execute(HttpServletRequest request,
-			HttpServletResponse response, ArrayList<String> more) throws ApiTypeException, ApiInternalException {
-
-		Object ret = null;
-		String type = more.get(RequestCodes.TYPE.code());
-		String service = request.getParameter("service");
-
-		if(type.equals("addr")) {
-			try {
-				ret = DistrictServices.getDistrictsFromAddress(more.get(RequestCodes.ADDRESS.code()), service);
-			} catch (Exception e) {
-				e.printStackTrace();
-				throw new ApiInternalException();
-			}
-		}
-		else if(type.equals("extended")) {
-			try {
-				ret = DistrictServices.getDistrictsFromAddress(
-							request.getParameter("addr2"),
-							request.getParameter("city"),
-							request.getParameter("state"),
-							request.getParameter("zip4"),
-							request.getParameter("zip5"),
-						service);
-			} catch (Exception e) {
-				e.printStackTrace();
-				throw new ApiInternalException();
-			}
-		}
-		else if(type.equals("latlon")) {
-			try {
-				ret = DistrictServices.getDistrictsFromPoint(more.get(RequestCodes.LATLON.code()), service);
-			} catch (Exception e) {
-				e.printStackTrace();
-				throw new ApiInternalException();
-			}
-		}
-		else {
-			throw new ApiTypeException(type);
-		}
-
-		return ret;
-	}
-	*/
+    catch (UnsupportedEncodingException e) {
+      throw new ApiInternalException("UTF-8 unsupported uncoding.", e);
+    }
+    catch (IOException e) {
+      throw new ApiInternalException("Unable to connect to database.", e);
+    }
+    catch (Exception e) {
+      throw new ApiInternalException("Error retrieving object.", e);
+    }
+    finally {
+      db.close();
+    }
+  } // getDistricts()
 }
