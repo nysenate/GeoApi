95375dce502094c44cddeea2a6bc90b81607f6f8
Ken Zalewski <zalewski.senate@gmail.com>
2013-02-07 04:55:13 -0500

    Some code reformatting.

--- src/main/java/gov/nysenate/sage/api/methods/RevGeoMethod.java
+++ src/main/java/gov/nysenate/sage/api/methods/RevGeoMethod.java
@@ -10,23 +10,24 @@ import java.util.ArrayList;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-public class RevGeoMethod extends ApiExecution {
-	@Override
-	public Object execute(HttpServletRequest request, HttpServletResponse response, ArrayList<String> more) throws ApiTypeException, ApiInternalException {
 
-		String type = more.get(RequestCodes.TYPE.code());
-		if(!type.equals("latlon"))
-		    throw new ApiTypeException(type);
+public class RevGeoMethod extends ApiExecution
+{
+  @Override
+  public Object execute(HttpServletRequest request, HttpServletResponse response, ArrayList<String> more) throws ApiTypeException, ApiInternalException
+  {
+    String type = more.get(RequestCodes.TYPE.code());
+    if (!type.equals("latlon")) {
+      throw new ApiTypeException(type);
+    }
 
-		try {
-		    Object ret = null;
-		    String service = request.getParameter("service");
-			ret = GeoCode.getReverseGeoCodedResponse(more.get(RequestCodes.LATLON.code()), service);
-			return ret;
-		}
-		catch (Exception e) {
-			e.printStackTrace();
-			throw new ApiInternalException();
-		}
-	}
+    try {
+      String service = request.getParameter("service");
+      return GeoCode.getReverseGeoCodedResponse(more.get(RequestCodes.LATLON.code()), service);
+    }
+    catch (Exception e) {
+      e.printStackTrace();
+      throw new ApiInternalException();
+    }
+  } // execute()
 }
