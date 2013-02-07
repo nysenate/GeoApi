594df246a9e4cb54e335a9a7d1ef0b3e187fe795
Ken Zalewski <zalewski.senate@gmail.com>
2013-02-07 04:49:19 -0500

    Now using accessor methods on the Result object.

--- src/main/java/gov/nysenate/sage/api/methods/CityStateLookupMethod.java
+++ src/main/java/gov/nysenate/sage/api/methods/CityStateLookupMethod.java
@@ -15,39 +15,48 @@ import java.util.ArrayList;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-public class CityStateLookupMethod extends ApiExecution {
+
+public class CityStateLookupMethod extends ApiExecution
+{
     AddressService addressService;
 
-    public CityStateLookupMethod() throws Exception {
+    public CityStateLookupMethod() throws Exception
+    {
         addressService = new AddressService();
     }
 
-	@Override
-	public Object execute(HttpServletRequest request, HttpServletResponse response, ArrayList<String> more) throws ApiException {
+
+    @Override
+    public Object execute(HttpServletRequest request, HttpServletResponse response, ArrayList<String> more) throws ApiException
+    {
         String type = more.get(RequestCodes.TYPE.code());
         Address address;
 
         System.out.println(type);
-        if(type.equals("extended")) {
+        if (type.equals("extended")) {
             address = new Address("","","","",request.getParameter("zip5"),"");
-        } else if (type.equals("zip")) {
+        }
+        else if (type.equals("zip")) {
             address = new Address("","","","",more.get(RequestCodes.ZIP.code()),"");
-        } else {
+        }
+        else {
             throw new ApiTypeException(type);
         }
 
-        Result result = addressService.lookupCityState(address,"usps");
+        Result result = addressService.lookupCityState(address, "usps");
 
-        if (result==null) {
+        if (result == null) {
             throw new ApiInternalException();
-        } else if (result.status_code.equals("0")) {
-            return new ValidateResponse(result.address);
-        } else {
+        }
+        else if (result.getStatus().equals("0")) {
+            return new ValidateResponse(result.getAddress());
+        }
+        else {
             String msg = "";
-            for (String m : result.messages) {
+            for (String m : result.getMessages()) {
                 msg += "\n"+m;
             }
             return new ErrorResponse(msg.toString());
         }
-	}
+    }
 }
