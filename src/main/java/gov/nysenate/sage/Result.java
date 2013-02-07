b75469da2b08d92c4bd980e3135d4d11957f6c7a
Ken Zalewski <zalewski.senate@gmail.com>
2013-02-07 04:46:17 -0500

    Converted all public member data to private visibility.  Added a bunch of accessor methods.

--- src/main/java/gov/nysenate/sage/Result.java
+++ src/main/java/gov/nysenate/sage/Result.java
@@ -1,9 +1,12 @@
 package gov.nysenate.sage;
 
 import java.util.ArrayList;
+import java.util.List;
+
 /**
  *
  * @author Graylin Kim
+ * @author Ken Zalewski
  *
  * A generic Result structure passed back from all services.
  *
@@ -19,10 +22,118 @@ import java.util.ArrayList;
  * return an array of results even in the face of partial failure while retaining all
  * relevant error details for further action by the caller.
  */
-public class Result {
-    public Address address;
-    public ArrayList<Address> addresses = new ArrayList<Address>();
-    public ArrayList<String> messages = new ArrayList<String>();
-    public String status_code = "0";
-    public String source = "";
+
+public class Result
+{
+  private Address m_address;
+  private ArrayList<Address> m_addresses;
+  private ArrayList<String> m_messages;
+  private String m_status;
+  private String m_source;
+
+
+  public Result(String status, String src)
+  {
+    m_status = status;
+    m_source = src;
+    m_address = null;
+    m_addresses = new ArrayList<Address>();
+    m_messages = new ArrayList<String>();
+  } // Result()
+
+
+  public Result(String src)
+  {
+    this("0", src);
+  } // Result()
+
+
+  public Result()
+  {
+    this("0", "");
+  } // Result()
+
+
+  public Address getAddress()
+  {
+    return m_address;
+  }
+
+
+  public void setAddress(Address addr)
+  {
+    m_address = addr;
+  } // setAddress()
+
+
+  public List<Address> getAddresses()
+  {
+    return m_addresses;
+  }
+
+
+  public Address getFirstAddress()
+  {
+    return m_addresses.get(0);
+  }
+
+
+  public void setAddresses(ArrayList<Address> addrs)
+  {
+    m_addresses = addrs;
+  } // setAddresses()
+
+
+  public void addAddress(Address addr)
+  {
+    m_addresses.add(addr);
+  } // addAddress()
+
+
+  public List<String> getMessages()
+  {
+    return m_messages;
+  } // getMessages()
+
+
+  public String getFirstMessage()
+  {
+    return m_messages.get(0);
+  }
+
+
+  public void setMessages(ArrayList<String> msgs)
+  {
+    m_messages = msgs;
+  } // setMessages()
+
+
+  public void addMessage(String msg)
+  {
+    m_messages.add(msg);
+  } // addMessage()
+
+
+  public String getStatus()
+  {
+    return m_status;
+  } // getStatus()
+
+
+  public void setStatus(String status)
+  {
+    m_status = status;
+  } // setStatus()
+
+
+  public String getSource()
+  {
+    return m_source;
+  } // getSource()
+
+
+  public void setSource(String src)
+  {
+    m_source = src;
+  } // setSource()
 }
