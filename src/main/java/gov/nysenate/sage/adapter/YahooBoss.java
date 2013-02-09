f5ef88c28b0ada888375a2beb1112d5ed1ca5979
Ken Zalewski <zalewski.senate@gmail.com>
2013-02-09 07:58:46 -0500

    Using JSON as response format from Yahoo.  Removed all DOM, SAX, and XPATH classes.  Properly using yahoo.boss.url.

--- src/main/java/gov/nysenate/sage/adapter/YahooBoss.java
+++ src/main/java/gov/nysenate/sage/adapter/YahooBoss.java
@@ -21,13 +21,6 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.xpath.XPath;
-import javax.xml.xpath.XPathConstants;
-import javax.xml.xpath.XPathExpressionException;
-import javax.xml.xpath.XPathFactory;
-
 import oauth.signpost.OAuthConsumer;
 import oauth.signpost.basic.DefaultOAuthConsumer;
 import oauth.signpost.exception.OAuthCommunicationException;
@@ -36,19 +29,16 @@ import oauth.signpost.exception.OAuthMessageSignerException;
 
 import org.apache.commons.io.IOUtils;
 import org.apache.log4j.Logger;
-import org.w3c.dom.Document;
-import org.w3c.dom.Node;
-import org.w3c.dom.NodeList;
-import org.xml.sax.SAXException;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 
 public class YahooBoss implements GeocodeInterface, Observer
 {
   private static final String DEFAULT_BASE_URL = "http://yboss.yahooapis.com/geo/placefinder";
-  private final Logger logger;
-  private final XPath xpath;
-  private final DocumentBuilder xmlBuilder;
-
+  private final Logger m_logger;
   private String m_baseUrl;
   private String m_consumerKey;
   private String m_consumerSecret;
@@ -56,19 +46,19 @@ public class YahooBoss implements GeocodeInterface, Observer
 
   public class ParallelRequest implements Callable<Result>
   {
-    public final YahooBoss yahooBoss;
-    public final Address address;
+    private final YahooBoss m_yahooBoss;
+    private final Address m_address;
 
     ParallelRequest(YahooBoss yahooBoss, Address address)
     {
-      this.yahooBoss = yahooBoss;
-      this.address = address;
+      m_yahooBoss = yahooBoss;
+      m_address = address;
     }
 
     @Override
     public Result call() throws GeoException
     {
-      return yahooBoss.geocode(address);
+      return m_yahooBoss.geocode(m_address);
     }
   } // ParallelRequest
 
@@ -77,10 +67,8 @@ public class YahooBoss implements GeocodeInterface, Observer
   {
     Config.notify(this);
     configure();
-    logger = Logger.getLogger(this.getClass());
-    xmlBuilder = DocumentBuilderFactory.newInstance().newDocumentBuilder();
-    xpath = XPathFactory.newInstance().newXPath();
-    logger.info("Initialized Yahoo Adapter");
+    m_logger = Logger.getLogger(this.getClass());
+    m_logger.info("Initialized Yahoo Adapter");
   }
 
   public void update(Observable o, Object arg)
@@ -91,7 +79,8 @@ public class YahooBoss implements GeocodeInterface, Observer
 
   // Yahoo doesn't implement batch geocoding so we use the single address geocoding
   // method in parallel for performance improvements on our end.
-  public ArrayList<Result> geocode(ArrayList<Address> addresses, Address.TYPE hint) throws GeoException {
+  public ArrayList<Result> geocode(ArrayList<Address> addresses, Address.TYPE hint) throws GeoException
+  {
     ArrayList<Result> results = new ArrayList<Result>();
     ExecutorService executor = Executors.newFixedThreadPool(5);
     ArrayList<Future<Result>> futureResults = new ArrayList<Future<Result>>();
@@ -103,20 +92,22 @@ public class YahooBoss implements GeocodeInterface, Observer
     for (Future<Result> result : futureResults) {
       try {
         results.add(result.get());
-      } catch (InterruptedException e) {
+      }
+      catch (InterruptedException e) {
         throw new GeoException(e);
-      } catch (ExecutionException e) {
+      }
+      catch (ExecutionException e) {
         throw new GeoException(e.getCause());
       }
     }
     executor.shutdown();
     return results;
-  }
+  } // geocode()
+
 
   @Override
   public Result geocode(Address address) throws GeoException
   {
-    Document body = null;
     Result result = null;
 
     if (address == null) {
@@ -125,94 +116,77 @@ public class YahooBoss implements GeocodeInterface, Observer
 
     try {
       // Parse the API response
-      String urlText = "http://yboss.yahooapis.com/geo/placefinder?location="+URLEncoder.encode(address.as_raw(), "UTF-8").replace("+", "%20");
+      String urlText = m_baseUrl+"?flags=J&location="+URLEncoder.encode(address.as_raw(), "UTF-8").replace("+", "%20");
       result = new Result(urlText);
-      logger.info(urlText);
+      m_logger.info(urlText);
 
       URL u = new URL(urlText);
       HttpURLConnection uc = (HttpURLConnection)u.openConnection();
       OAuthConsumer consumer = new DefaultOAuthConsumer(m_consumerKey, m_consumerSecret);
       consumer.sign(uc);
+      int httpRespCode = uc.getResponseCode();
 
-      if (uc.getResponseCode() != HttpURLConnection.HTTP_OK) {
-        result.setStatus(String.valueOf(uc.getResponseCode()));
+      if (httpRespCode != HttpURLConnection.HTTP_OK) {
+        result.setStatus(String.valueOf(httpRespCode));
         result.addMessage(IOUtils.toString(uc.getErrorStream()));
         return result;
       }
 
-      synchronized (xmlBuilder) {
-        body = xmlBuilder.parse(uc.getInputStream());
-      }
-
-      String status = xpath.evaluate("/bossresponse/@responsecode", body);
+      String body = IOUtils.toString(uc.getInputStream());
+      JSONObject jsonObj = new JSONObject(body).getJSONObject("bossresponse");
+      String status = jsonObj.getString("responsecode");
       if (!status.equals("200")) {
         result.setStatus(status);
-        // TODO: There are no examples of what an error will look like
-        result.addMessage(xpath.evaluate("ResultSet/ErrorMessage", body));
+        result.addMessage(jsonObj.getString("reason"));
         return result;
       }
 
-      NodeList resultset = (NodeList)xpath.evaluate("/bossresponse/placefinder/results/result", body, XPathConstants.NODESET);
-      for (int i = 0; i < resultset.getLength(); i++) {
-        Node location = resultset.item(i);
-        String street = xpath.evaluate("line1", location);
-        String city = xpath.evaluate("city", location);
-        String state = xpath.evaluate("statecode", location);
-        String zip_code = xpath.evaluate("uzip", location);
-        int quality = Integer.valueOf(xpath.evaluate("quality", location));
-        double lat = (Double)xpath.evaluate("offsetlat", location, XPathConstants.NUMBER);
-        double lng = (Double)xpath.evaluate("offsetlon", location, XPathConstants.NUMBER);
-
-        Address resultAddress = new Address(street, city, state, zip_code);
-        resultAddress.setGeocode(lat, lng, quality);
-        result.addAddress(resultAddress);
+      JSONArray resultSet = jsonObj.getJSONObject("placefinder").getJSONArray("results");
+      for (int i = 0; i < resultSet.length(); i++) {
+        Address resultAddr = buildAddress(resultSet.getJSONObject(i));
+        result.addAddress(resultAddr);
       }
 
       return result;
     }
     catch (UnsupportedEncodingException e) {
       String msg = "UTF-8 encoding not supported!?";
-      logger.error(msg);
+      m_logger.error(msg);
       throw new GeoException(msg);
     }
     catch (MalformedURLException e) {
       String msg = "Malformed URL '"+result.getSource()+"', check API key and address values.";
-      logger.error(msg, e);
+      m_logger.error(msg, e);
       throw new GeoException(msg, e);
     }
     catch (IOException e) {
       String msg = "Error opening API resource '"+result.getSource()+"'";
-      logger.error(msg, e);
+      m_logger.error(msg, e);
       result.setStatus("500");
       result.addMessage(e.getMessage());
       return result;
     }
-    catch (SAXException e) {
-      String msg = "Malformed XML response for '"+result.getSource()+"'";
-      logger.error(msg, e);
-      throw new GeoException(msg, e);
-    }
-    catch (XPathExpressionException e) {
-      String msg = "Unexpected XML Schema\n\n"+body.toString();
-      logger.error(msg, e);
-      throw new GeoException(msg ,e);
-    }
     catch (OAuthMessageSignerException e) {
       String msg = "OAuthMessageSignerException";
-      logger.error(msg, e);
+      m_logger.error(msg, e);
       throw new GeoException(msg ,e);
     }
     catch (OAuthExpectationFailedException e) {
       String msg = "OAuthExpectationFailedException";
-      logger.error(msg, e);
+      m_logger.error(msg, e);
       throw new GeoException(msg ,e);
     }
     catch (OAuthCommunicationException e) {
       String msg = "OAuthCommunicationException";
-      logger.error(msg, e);
+      m_logger.error(msg, e);
       throw new GeoException(msg ,e);
     }
-  }
+    catch (JSONException jsonEx) {
+      String msg = "Error while processing JSON result";
+      m_logger.error(msg, jsonEx);
+      throw new GeoException(msg, jsonEx);
+    }
+  } // geocode()
 
 
   private void configure() {
@@ -224,4 +198,20 @@ public class YahooBoss implements GeocodeInterface, Observer
       m_baseUrl = DEFAULT_BASE_URL;
     }
   } //configure()
+
+
+  private Address buildAddress(JSONObject jsonRes) throws JSONException
+  {
+    String street = jsonRes.optString("line1", null);
+    String city = jsonRes.optString("city", null);
+    String state = jsonRes.optString("statecode", null);
+    String zip_code = jsonRes.optString("postal", null);
+    int quality = jsonRes.optInt("quality", 0);
+    double lat = jsonRes.optDouble("offsetlat", 0.0);
+    double lng = jsonRes.optDouble("offsetlon", 0.0);
+
+    Address resultAddress = new Address(street, city, state, zip_code);
+    resultAddress.setGeocode(lat, lng, quality);
+    return resultAddress;
+  } // buildAddress()
 }
