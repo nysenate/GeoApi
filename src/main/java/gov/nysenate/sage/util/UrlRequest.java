cc08de85664b7991f62ce5491e5b58eda52604ae
Ash Islam <ashislam858@gmail.com>
2013-02-25 15:28:16 -0500

    Added common url request utlity class

---
+++ src/main/java/gov/nysenate/sage/util/UrlRequest.java
@@ -0,0 +1,38 @@
+package gov.nysenate.sage.util;
+
+import org.apache.commons.io.IOUtils;
+import org.apache.log4j.Logger;
+
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class UrlRequest
+{
+    public static Logger logger = Logger.getLogger(UrlRequest.class);
+
+    /**
+     * Connects to a url and retrieves the body response in String representation.
+     * This function can alternatively be implemented using the code snippet below
+     * but this produces unnecessarily detailed logs.
+     * <code>
+     *     Content content = Request.Get(url).execute().returnContent();
+     *     String response = content.asString();
+     * </code>
+     *
+     * @param url   Url request string
+     * @return      String containing response
+     * @throws MalformedURLException
+     * @throws IOException
+     */
+    public static String getResponseFromUrl(String url) throws MalformedURLException, IOException
+    {
+        URL u = new URL(url);
+        logger.debug("Requesting connection to " + url.toString());
+        HttpURLConnection uc = (HttpURLConnection)u.openConnection();
+        String response = IOUtils.toString(uc.getInputStream());
+        logger.debug("Retrieved response stream of length " + response.length());
+        return response;
+    }
+}
