ec03249bbafb4e472092d47d82a4f7e0b075cf6c
Ash Islam <ashislam858@gmail.com>
2013-02-27 15:19:27 -0500

    Removed ruby geocoder dependencies

--- rubygeocoder_dependencies/ruby-geo/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Webapp::Application.config.secret_token = '0111648044771e5fcd71f8c67a8c1c39ca8d41dbd22995e39ae3428f1e81700626e40e41acba20145a609fe4fb12109e04576d326248d8c489fa901ebc9ef654'
