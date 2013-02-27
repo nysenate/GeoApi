a3fd7b2119030640595700ee8e8885535cbef0ed
Ash Islam <ashislam858@gmail.com>
2013-02-27 13:20:46 -0500

    removed address from Point. renamed getters

--- src/main/java/gov/nysenate/sage/model/geo/Point.java
+++ src/main/java/gov/nysenate/sage/model/geo/Point.java
@@ -1,107 +1,66 @@
 package gov.nysenate.sage.model.geo;
 
-import com.thoughtworks.xstream.annotations.XStreamAlias;
-import com.google.gson.annotations.SerializedName;
-
 /**
+ * Simple point implementation
  * @author Ken Zalewski
  */
 
-@XStreamAlias("point")
 public class Point
 {
-  @XStreamAlias("lat")
-  @SerializedName("lat")
-  private double m_latitude;
-  @XStreamAlias("lon")
-  @SerializedName("lon")
-  private double m_longitude;
-  /** REMOVE address: it does not belong in a point class */
-  @XStreamAlias("address")
-  @SerializedName("address")
-  private String m_address;
-  
+    private double lat;
+    private double lon;
 
-  /**
-   * Construct a Point using the provided latitude and longitude.
-   *
-   * @param lat the latitude as a Double number
-   * @param lon the longitude as a Double number
-   */
-  public Point(double lat, double lon)
-  {
-    this(lat, lon, null);
-  } // Point()
-  
+    /**
+     * Construct a Point using the provided latitude and longitude.
+     *
+     * @param lat the latitude as a Double number
+     * @param lon the longitude as a Double number
+     */
+    public Point(double lat, double lon)
+    {
+        this.lat = lat;
+        this.lon = lon;
+    }
 
-  /**
-   * Construct a Point using the provided latitude, longitude, and address.
-   *
-   * @param lat the latitude as a Double number
-   * @param lon the longitude as a Double number
-   * @param address a postal address as a string
-   */
-  public Point(double lat, double lon, String address)
-  {
-    m_latitude = lat;
-    m_longitude = lon;
-    m_address = address;
-  } // Point()
+    /**
+     * Retrieve the latitude associated with this Point.
+     *
+     * @return the latitude as a double-precision number
+     */
+    public double getLat()
+    {
+        return lat;
+    }
 
-  
-  /**
-   * Retrieve the latitude associated with this Point.
-   *
-   * @return the latitude as a double-precision number
-   */
-  public double getLatitude()
-  {
-    return m_latitude;
-  } // getLatitude()
+    /**
+     * Retrieve the longitude associated with this Point.
+     *
+     * @return the longitude as a double-precision number
+     */
+    public double getLon()
+    {
+        return lon;
+    }
 
+    /**
+     * Get the string representation of this Point.  The string representation
+     * is of the form <latitude>,<longitude>
+     *
+     * @return the string representation of this Point
+     */
+    public String toString()
+    {
+        return getLat()+","+ getLon();
+    }
 
-  /**
-   * Retrieve the longitude associated with this Point.
-   *
-   * @return the longitude as a double-precision number
-   */
-  public double getLongitude()
-  {
-    return m_longitude;
-  } // getLongitude()
-
-
-  /**
-   * Retrieve the address associated with this Point.
-   *
-   * @return the address as a string
-   */
-  public String getAddress()
-  {
-    return m_address;
-  } // getAddress()
-
-
-  /**
-   * Get the string representation of this Point.  The string representation
-   * is of the form <latitude>,<longitude>
-   *
-   * @return the string representation of this Point
-   */
-  public String toString()
-  {
-    return getLatitude()+","+getLongitude();
-  } // toString()
-
-
-  /**
-   * Get the JSON representation of this Point.  The JSON representation
-   * is of the form [<latitude>,<longitude>]
-   *
-   * @return a string containing the JSON representation of this Point
-   */
-  public String toJson()
-  {
-    return "["+this.toString()+"]";
-  } // toJson()
-} // Point
+    /**
+     * Get the JSON representation of this Point.  The JSON representation
+     * is of the form [<latitude>,<longitude>]
+     *
+     * @return a string containing the JSON representation of this Point
+     */
+    public String toJson()
+    {
+        return "["+this.toString()+"]";
+    }
+}
