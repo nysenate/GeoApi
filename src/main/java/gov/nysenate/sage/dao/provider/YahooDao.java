be4a3580bff6a72be298e955ecbb19c3a2851de5
Ash Islam <ashislam858@gmail.com>
2013-03-04 15:11:40 -0500

    Refactored yahoo provider: abstracted out a dao and implemented better error checking

--- src/main/java/gov/nysenate/sage/dao/provider/YahooDao.java
+++ src/main/java/gov/nysenate/sage/dao/provider/YahooDao.java
@@ -1,9 +1,172 @@
 package gov.nysenate.sage.dao.provider;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import gov.nysenate.sage.model.address.Address;
+import gov.nysenate.sage.model.address.GeocodedAddress;
+import gov.nysenate.sage.model.geo.Geocode;
+import gov.nysenate.sage.model.geo.GeocodeQuality;
+import gov.nysenate.sage.model.geo.Point;
+import gov.nysenate.sage.util.UrlRequest;
+import org.apache.log4j.Logger;
+
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.net.MalformedURLException;
+import java.net.URLEncoder;
+
 /**
+ *  Provides a data abstraction layer for performing Yahoo Placefinder requests.
+ *  This is the free non-commercial Yahoo service that utilizes YQL queries.
  *
+ *  Documentation available here:
+ *  http://developer.yahoo.com/boss/geo/docs/free_YQL.html#table_pf
  */
 public class YahooDao
 {
+    private static final String DEFAULT_BASE_URL = "http://query.yahooapis.com/v1/public/yql";
+    private static final String SET_QUERY_AS = "?format=json&q=";
+    private static final String GEOCODE_QUERY = "select * from geo.placefinder where text=\"%s\"";
+    private static final String REVERSE_GEO_QUERY = "select * from geo.placefinder where text=\"%f,%f\" and gflags=\"R\"";
 
+    private Logger logger = Logger.getLogger(YahooDao.class);
+    private String baseUrl;
+    private ObjectMapper objectMapper;
+
+    public YahooDao(){
+        this.objectMapper = new ObjectMapper();
+    }
+
+    public String getBaseUrl() {
+        return (this.baseUrl != null && !this.baseUrl.isEmpty()) ? this.baseUrl : DEFAULT_BASE_URL;
+    }
+
+    public void setBaseUrl(String baseUrl)
+    {
+        if (baseUrl != null && !baseUrl.isEmpty()){
+            this.baseUrl = baseUrl;
+        }
+    }
+
+    /**
+     * This method performs geocoding.
+     * Retrieves a GeocodedAddress given an Address using Yahoo.
+     *
+     * @param address   Address to geocode
+     * @return          GeocodedAddress containing best matched Geocode.
+     */
+    public GeocodedAddress getGeocodedAddress(Address address)
+    {
+        GeocodedAddress geocodedAddress = null;
+
+        try {
+            String formattedQuery = URLEncoder.encode(String.format(GEOCODE_QUERY, address.toString()), "UTF-8");
+            String url = getBaseUrl() + SET_QUERY_AS + formattedQuery;
+            geocodedAddress = getGeocodedAddress(url);
+        }
+        catch (UnsupportedEncodingException ex) {
+            logger.error("UTF-8 encoding not supported!?", ex);
+        }
+        return geocodedAddress;
+    }
+
+    /**
+     * This method performs reverse geocoding.
+     * Retrieves a GeocodedAddress given a Point using Yahoo.
+     *
+     * @param point Point to reverse geocode.
+     * @return      GeocodedAddress containing best matched Address.
+     */
+    public GeocodedAddress getGeocodedAddress(Point point)
+    {
+        GeocodedAddress geocodedAddress = null;
+        try {
+            String formattedQuery = URLEncoder.encode(String.format(REVERSE_GEO_QUERY, point.getLat(), point.getLon()), "UTF-8");
+            String url = getBaseUrl() + SET_QUERY_AS + formattedQuery;
+            geocodedAddress = getGeocodedAddress(url);
+        }
+        catch(UnsupportedEncodingException ex){
+            logger.error("UTF-8 encoding not supported!?", ex);
+        }
+        return geocodedAddress;
+    }
+
+    /**
+     * Abstraction method that makes a request to the given Yahoo url, parses
+     * the response, and returns a GeocodedAddress.
+     * @param url   The query url
+     *              e.g. http://query.yahooapis.com/v1/public/yql?format=json&q=blah..
+     * @return      GeocodedAddress, or null on parse failure
+     */
+    private GeocodedAddress getGeocodedAddress(String url)
+    {
+        GeocodedAddress geocodedAddress = null;
+        try {
+            String json = UrlRequest.getResponseFromUrl(url);
+            JsonNode rootNode = objectMapper.readTree(json).get("query");
+            JsonNode resultsNode = rootNode.get("results");
+            int resultCount = rootNode.get("count").asInt();
+
+            /** Retrieve the first result only. We can assume that results are ranked. */
+            if (resultCount == 1) {
+                geocodedAddress = getGeocodedAddressFromResultNode(resultsNode.get("Result"));
+            }
+            else if (resultCount > 1) {
+                geocodedAddress = getGeocodedAddressFromResultNode(resultsNode.get("Result").get(0));
+            }
+        }
+        catch (MalformedURLException ex) {
+            logger.error("Malformed URL! ", ex);
+        }
+        catch (IOException ex) {
+            logger.error("Error opening API resource!", ex);
+        }
+        return geocodedAddress;
+    }
+
+    /**
+     * Parses and returns GeocodedAddress from result JSON node.
+     *
+     * The resultNode is a reference to the "Result" node depicted by the following
+     * response structure that is returned from yahoo:
+     *
+     * {"query": { ... "results": { "Result": { ..result data.. }}}}
+     *
+     * @param resultNode    The "Result" node
+     * @return              The GeocodedAddress created from the "Result" data
+     */
+    private GeocodedAddress getGeocodedAddressFromResultNode(JsonNode resultNode)
+    {
+        String street = resultNode.get("line1").asText();
+        String city = resultNode.get("city").asText();
+        String state = resultNode.get("statecode").asText();
+        String postal = resultNode.get("postal").asText();
+        GeocodeQuality quality = resolveGeocodeQuality(resultNode.get("quality").asInt());
+        double lat = resultNode.get("latitude").asDouble(0.0);
+        double lng = resultNode.get("longitude").asDouble(0.0);
+
+        Address resultAddress = new Address(street, city, state, postal);
+        Geocode resultGeocode = new Geocode(new Point(lat, lng), quality, this.getClass().getSimpleName());
+        GeocodedAddress geocodedAddress = new GeocodedAddress(resultAddress, resultGeocode);
+        return geocodedAddress;
+    }
+
+    /**
+     * Yahoo has a detailed list of quality codes. Here we can condense the codes into
+     * our basic GeocodeQuality levels.
+     * @param quality   Integer code provided by Yahoo response
+     * @return          Corresponding GeocodeQuality
+     */
+    private GeocodeQuality resolveGeocodeQuality(int quality)
+    {
+        if ( quality == 99 || quality == 90 ) return GeocodeQuality.POINT;
+        if (quality >= 80) return GeocodeQuality.HOUSE;
+        if (quality >= 74) return GeocodeQuality.ZIP_EXT;
+        if (quality >= 70) return GeocodeQuality.STREET;
+        if (quality >= 59) return GeocodeQuality.ZIP;
+        if (quality >= 39) return GeocodeQuality.CITY;
+        if (quality >= 29) return GeocodeQuality.COUNTY;
+        if (quality >= 19) return GeocodeQuality.STATE;
+        return GeocodeQuality.NOMATCH;
+    }
 }
--- src/main/java/gov/nysenate/sage/provider/Yahoo.java
+++ src/main/java/gov/nysenate/sage/provider/Yahoo.java
@@ -1,53 +1,40 @@
 package gov.nysenate.sage.provider;
 
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
+import gov.nysenate.sage.dao.provider.YahooDao;
 import gov.nysenate.sage.factory.ApplicationFactory;
 import gov.nysenate.sage.model.address.Address;
 import gov.nysenate.sage.model.address.GeocodedAddress;
-import gov.nysenate.sage.model.geo.Geocode;
-import gov.nysenate.sage.model.geo.GeocodeQuality;
 import gov.nysenate.sage.model.geo.Point;
 import gov.nysenate.sage.model.result.GeocodeResult;
+import gov.nysenate.sage.model.result.ResultStatus;
 import gov.nysenate.sage.service.geo.GeocodeService;
 import gov.nysenate.sage.service.geo.ParallelGeocodeService;
 import gov.nysenate.sage.util.Config;
-import org.apache.http.client.fluent.Content;
-import org.apache.http.client.fluent.Request;
 import org.apache.log4j.Logger;
-import org.json.JSONException;
 
-import java.io.IOException;
-import java.io.UnsupportedEncodingException;
-import java.net.MalformedURLException;
-import java.net.URLEncoder;
 import java.util.ArrayList;
 import java.util.Observable;
 import java.util.Observer;
 
 public class Yahoo implements GeocodeService, Observer
 {
-    private static final String DEFAULT_BASE_URL = "http://query.yahooapis.com/v1/public/yql";
     private final Logger logger = Logger.getLogger(Yahoo.class);
+    private YahooDao yahooDao;
     private Config config;
-    private String baseUrl;
-    private ObjectMapper objectMapper;
 
     public Yahoo()
     {
         this.config = ApplicationFactory.getConfig();
-        this.objectMapper = new ObjectMapper();
+        this.yahooDao = new YahooDao();
         configure();
+        config.notifyOnChange(this);
         logger.info("Initialized Yahoo Adapter");
     }
 
     private void configure()
     {
-        baseUrl = config.getValue("yahoo.url");
-        if (baseUrl.isEmpty()) {
-            baseUrl = DEFAULT_BASE_URL;
-        }
-        config.notifyOnChange(this);
+        String baseUrl = config.getValue("yahoo.url");
+        this.yahooDao.setBaseUrl(baseUrl);
     }
 
     public void update(Observable o, Object arg)
@@ -58,12 +45,33 @@ public class Yahoo implements GeocodeService, Observer
     @Override
     public GeocodeResult geocode(Address address)
     {
+        GeocodeResult geocodeResult = new GeocodeResult(this.getClass());
+
+        /** Proceed only on valid input */
         if (address == null) {
-            return null;
+            geocodeResult.setStatusCode(ResultStatus.MISSING_ADDRESS);
+            return geocodeResult;
+        }
+        else if (address.isEmpty()){
+            geocodeResult.setStatusCode(ResultStatus.INSUFFICIENT_ADDRESS);
+            return geocodeResult;
         }
 
-        String yql = "select * from geo.placefinder where text=\"" + address.toString() + "\"";
-        return getGeocodeResultFromYahoo(yql);
+        GeocodedAddress geocodedAddress = this.yahooDao.getGeocodedAddress(address);
+
+        /** Handle error cases or set GeocodedAddress if successful */
+        if (geocodedAddress != null){
+            if (geocodedAddress.isGeocoded()){
+                geocodeResult.setGeocodedAddress(geocodedAddress);
+            }
+            else {
+                geocodeResult.setStatusCode(ResultStatus.NO_GEOCODE_RESULT);
+            }
+        }
+        else {
+            geocodeResult.setStatusCode(ResultStatus.RESPONSE_PARSE_ERROR);
+        }
+        return geocodeResult;
     }
 
     /**
@@ -79,108 +87,27 @@ public class Yahoo implements GeocodeService, Observer
     @Override
     public GeocodeResult reverseGeocode(Point point)
     {
+        GeocodeResult geocodeResult = new GeocodeResult(this.getClass());
+
+        /** Validate the input */
         if (point == null) {
-            return null;
+            geocodeResult.setStatusCode(ResultStatus.MISSING_POINT);
         }
 
-        String yql = "select * from geo.placefinder where text=\"" + point.getLat() + "," + point.getLon() +
-                     "\" and gflags=\"R\"";
+        GeocodedAddress geocodedAddress = this.yahooDao.getGeocodedAddress(point);
 
-        return getGeocodeResultFromYahoo(yql);
-    }
-
-    private GeocodeResult getGeocodeResultFromYahoo(String yql)
-    {
-        GeocodeResult geocodeResult = new GeocodeResult();
-        GeocodedAddress geocodedAddress = null;
-
-        try {
-            String encodedYql = URLEncoder.encode(yql, "UTF-8");
-            String url = baseUrl +"?format=json&q="+encodedYql;
-            logger.debug(url);
-
-            Content content = Request.Get(url).execute().returnContent();
-            String json = content.asString();
-
-            JsonNode jsonNode = objectMapper.readTree(json).get("query");
-            int resultCount = jsonNode.get("count").asInt();
-            JsonNode resultsNode = jsonNode.get("results");
-
-            /** Retrieve the first result */
-            if (resultCount == 1) {
-                geocodedAddress = getGeocodedAddressFromJson(resultsNode.get("Result"));
-            }
-            else if (resultCount > 1) {
-                geocodedAddress = getGeocodedAddressFromJson(resultsNode.get("Result").get(0));
-            }
-
-            if (geocodedAddress != null) {
+        /** Handle error cases or set GeocodedAddress if successful */
+        if (geocodedAddress != null){
+            if (geocodedAddress.isReverseGeocoded()){
                 geocodeResult.setGeocodedAddress(geocodedAddress);
             }
             else {
-                geocodeResult.addMessage("Failed to retrieve a geocoded address from the response");
+                geocodeResult.setStatusCode(ResultStatus.NO_REVERSE_GEOCODE_RESULT);
             }
-            return geocodeResult;
         }
-        catch (UnsupportedEncodingException ex) {
-            String msg = "UTF-8 encoding not supported!?";
-            logger.error(msg, ex);
+        else {
+            geocodeResult.setStatusCode(ResultStatus.RESPONSE_PARSE_ERROR);
         }
-        catch (MalformedURLException ex) {
-            String msg = "Malformed URL '" + geocodeResult.getSource() + "', check API key and address values.";
-            logger.error(msg, ex);
-        }
-        catch (IOException ex) {
-            String msg = "Error opening API resource '" + geocodeResult.getSource() + "'";
-            logger.error(msg, ex);
-        }
-        catch (NullPointerException ex){
-            logger.error("Error while parsing JSON result!", ex);
-        }
-        catch (Exception ex){
-            logger.error(ex.getMessage());
-        }
-        return null;
-    }
-
-    /**
-     * ...
-     * @param resultNode
-     * @return
-     * @throws JSONException
-     */
-    private GeocodedAddress getGeocodedAddressFromJson(JsonNode resultNode)
-    {
-        String street = resultNode.get("line1").asText();
-        String city = resultNode.get("city").asText();
-        String state = resultNode.get("statecode").asText();
-        String postal = resultNode.get("postal").asText();
-        GeocodeQuality quality = resolveGeocodeQuality(resultNode.get("quality").asInt());
-        double lat = resultNode.get("latitude").asDouble(0.0);
-        double lng = resultNode.get("longitude").asDouble(0.0);
-
-        Address resultAddress = new Address(street, city, state, postal);
-        Geocode resultGeocode = new Geocode(new Point(lat, lng), quality, this.getClass().getSimpleName());
-        GeocodedAddress geocodedAddress = new GeocodedAddress(resultAddress, resultGeocode);
-        return geocodedAddress;
-    }
-
-    /**
-     * Yahoo has a detailed list of quality codes. Here we can condense the codes into
-     * our basic GeocodeQuality levels.
-     * @param quality   Int provided by Yahoo response
-     * @return          Corresponding GeocodeQuality
-     */
-    private GeocodeQuality resolveGeocodeQuality(int quality)
-    {
-        if ( quality == 99 || quality == 90 ) return GeocodeQuality.POINT;
-        if (quality >= 80) return GeocodeQuality.HOUSE;
-        if (quality >= 74) return GeocodeQuality.ZIP_EXT;
-        if (quality >= 70) return GeocodeQuality.STREET;
-        if (quality >= 59) return GeocodeQuality.ZIP;
-        if (quality >= 39) return GeocodeQuality.CITY;
-        if (quality >= 29) return GeocodeQuality.COUNTY;
-        if (quality >= 19) return GeocodeQuality.STATE;
-        return GeocodeQuality.NOMATCH;
+        return geocodeResult;
     }
 }
