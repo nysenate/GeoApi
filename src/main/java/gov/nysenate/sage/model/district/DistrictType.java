b1dcf9dbe340dd9590596f806c115662cbdfd1c0
Ash Islam <ashislam858@gmail.com>
2013-03-07 09:56:52 -0500

    Added new DistrictType implementation with name to type resolver (tested).

--- src/main/java/gov/nysenate/sage/model/district/DistrictType.java
+++ src/main/java/gov/nysenate/sage/model/district/DistrictType.java
@@ -1,6 +1,41 @@
 package gov.nysenate.sage.model.district;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public enum DistrictType
 {
-    ASSEMBLY,CONGRESSIONAL,COUNTY,ELECTION,SENATE,SCHOOL,TOWN;
+    ASSEMBLY,CONGRESSIONAL,COUNTY,ELECTION,SENATE,SCHOOL,TOWN,    // Standard Districts
+    WARD, CLEG, CITY, FIRE, VILLAGE;                              // Extended Districts
+
+    /** A Map container is used to associate type names with the enum type */
+    private static Map<String, DistrictType> resolveMap = new HashMap<>();
+    static {
+        for (DistrictType dt : DistrictType.getAllTypes()){
+            resolveMap.put(dt.name().toUpperCase(), dt);
+        }
+    }
+
+    public static List<DistrictType> getStandardTypes()
+    {
+        return Arrays.asList(ASSEMBLY, CONGRESSIONAL, COUNTY, ELECTION, SENATE, SCHOOL, TOWN);
+    }
+
+    public static List<DistrictType> getExtendedTypes()
+    {
+        return Arrays.asList(WARD, CLEG, CITY, FIRE, VILLAGE);
+    }
+
+    public static List<DistrictType> getAllTypes()
+    {
+        return Arrays.asList(DistrictType.values());
+    }
+
+    /** Returns the DistrictType that matches the string representation */
+    public static DistrictType resolveType(String type)
+    {
+        return resolveMap.get(type.toUpperCase());
+    }
 }
---
+++ src/test/java/gov/nysenate/sage/model/district/DistrictTypeTest.java
@@ -0,0 +1,56 @@
+package gov.nysenate.sage.model.district;
+
+import gov.nysenate.sage.TestBase;
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+
+import static gov.nysenate.sage.model.district.DistrictType.*;
+import static junit.framework.Assert.*;
+
+public class DistrictTypeTest extends TestBase
+{
+    @Test
+    public void getStandardTypesTest()
+    {
+        List<DistrictType> standardTypes = DistrictType.getStandardTypes();
+        List<DistrictType> expectedTypes = Arrays.asList(SENATE, CONGRESSIONAL, ASSEMBLY, SCHOOL, TOWN, COUNTY, ELECTION);
+        assertEquals(new HashSet<>(standardTypes), new HashSet<>(expectedTypes));
+    }
+
+    @Test
+    public void getExtendedTypesTest()
+    {
+        List<DistrictType> extendedTypes = DistrictType.getExtendedTypes();
+        List<DistrictType> expectedTypes = Arrays.asList(FIRE, WARD, CITY, CLEG, VILLAGE);
+        assertEquals(new HashSet<>(extendedTypes), new HashSet<>(expectedTypes));
+    }
+
+    @Test
+    public void getAllTypesTest()
+    {
+        List<DistrictType> allTypes = DistrictType.getAllTypes();
+        assertEquals(new HashSet<>(allTypes), new HashSet<>(Arrays.asList(DistrictType.values())));
+    }
+
+    @Test
+    public void resolveType()
+    {
+        DistrictType resolvedSenate = DistrictType.resolveType("senate");
+        DistrictType resolvedAssembly = DistrictType.resolveType("AsSemblY");
+        DistrictType resolvedCongressional = DistrictType.resolveType("CONGRESSIONAL");
+        DistrictType resolvedNull = DistrictType.resolveType("Something");
+
+        assertEquals(SENATE,resolvedSenate);
+        assertEquals(ASSEMBLY,resolvedAssembly);
+        assertEquals(CONGRESSIONAL,resolvedCongressional);
+        assertNull(resolvedNull);
+    }
+
+
+
+
+
+}
