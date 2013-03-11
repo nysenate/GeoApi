afe2b2154e190f936ce8d01c562336084baf2cd7
Ash Islam <ashislam858@gmail.com>
2013-03-11 14:06:29 -0400

    Removed 'Connect' ORM

--- src/main/java/gov/nysenate/sage/client/api/ApiError.java
+++ src/main/java/gov/nysenate/sage/client/api/ApiError.java
@@ -5,28 +5,4 @@ import gov.nysenate.sage.model.result.ResultStatus;
 import java.util.ArrayList;
 import java.util.List;
 
-public class ApiError
-{
-    protected ResultStatus status;
-    protected List<String> messages = new ArrayList<>();
-
-    public ApiError(ResultStatus errorStatus)
-    {
-        this.status = errorStatus;
-    }
-
-    public String getStatus()
-    {
-        return status.name();
-    }
-
-    public String getDesc()
-    {
-        return status.getDesc();
-    }
-
-    public List<String> getMessages()
-    {
-        return this.messages;
-    }
-}
+public class ApiError extends ApiResponse {}
