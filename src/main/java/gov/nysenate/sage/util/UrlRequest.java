f844e33095092dce2d4377fd4267fc1a8eeaeae1
Ash Islam <ashislam858@gmail.com>
2013-03-28 15:48:34 -0400

    Better logging in UrlRequest

--- src/main/java/gov/nysenate/sage/util/UrlRequest.java
+++ src/main/java/gov/nysenate/sage/util/UrlRequest.java
@@ -4,6 +4,7 @@ import org.apache.commons.io.IOUtils;
 import org.apache.log4j.Logger;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
@@ -28,11 +29,41 @@ public abstract class UrlRequest
      */
     public static String getResponseFromUrl(String url) throws IOException
     {
+        InputStream inputStream = getInputStreamFromUrl(url);
+        if (inputStream != null) {
+            String response = IOUtils.toString(inputStream);
+            logger.debug("Retrieved string of length " + response.length());
+            logger.trace("Response: " + response);
+            return response;
+        }
+        else {
+            return null;
+        }
+    }
+
+    /**
+     * Retrieves an input stream from a url resource
+     * @param url
+     * @return
+     * @throws IOException
+     */
+    public static InputStream getInputStreamFromUrl(String url) throws IOException
+    {
         URL u = new URL(url);
         logger.debug("Requesting connection to " + url.toString());
+
         HttpURLConnection uc = (HttpURLConnection)u.openConnection();
-        String response = IOUtils.toString(uc.getInputStream());
-        logger.debug("Retrieved response stream of length " + response.length());
-        return response;
+        int responseCode = uc.getResponseCode();
+        logger.debug("Connection replied with response code: " + responseCode);
+
+        if (responseCode >= 400) {
+            logger.error("Failed to get a successful response. Returning null input stream.");
+            return null;
+        }
+
+        InputStream inputStream = uc.getInputStream();
+        logger.debug("Retrieved input stream");
+        return inputStream;
     }
+
 }
