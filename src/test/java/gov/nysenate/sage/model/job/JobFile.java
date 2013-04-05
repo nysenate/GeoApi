efc8a663844b9e6ae07f40acce496a065c12dd7f
Ash Islam <ashislam858@gmail.com>
2013-04-05 00:22:27 -0400

    Removed old bulk processing code

---
+++ src/test/java/gov/nysenate/sage/model/job/JobFile.java
@@ -0,0 +1,11 @@
+package gov.nysenate.sage.model.job;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: ash
+ * Date: 4/4/13
+ * Time: 5:22 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class JobFile {
+}
