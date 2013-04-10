b8ab80d6f42b9aa196e745c2681552f25454bd57
Ash Islam <ashislam858@gmail.com>
2013-04-10 11:41:19 -0400

    Updated example properties Added configurable TLS/SSL properties

---
+++ src/main/java/gov/nysenate/sage/model/job/JobBatch.java
@@ -0,0 +1,11 @@
+package gov.nysenate.sage.model.job;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: ash
+ * Date: 4/10/13
+ * Time: 9:35 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class JobBatch {
+}
---
+++ src/test/java/gov/nysenate/sage/util/MailerTest.java
@@ -0,0 +1,11 @@
+package gov.nysenate.sage.util;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: ash
+ * Date: 4/9/13
+ * Time: 10:49 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class MailerTest {
+}
