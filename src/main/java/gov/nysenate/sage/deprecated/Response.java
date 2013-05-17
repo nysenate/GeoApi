de27d4499149b910d05626221b86b23b38749e72
Ash Islam <ashislam858@gmail.com>
2013-05-17 12:28:36 -0400

    Deleted all deprecated files

--- src/main/java/gov/nysenate/sage/deprecated/Response.java
+++
@@ -1,5 +0,0 @@
-package gov.nysenate.sage.deprecated;
-
-public class Response {
-
-}
--- src/main/java/gov/nysenate/sage/deprecated/districts/DistrictType.java
+++
@@ -1,8 +0,0 @@
-package gov.nysenate.sage.deprecated.districts;
-
-@Deprecated
-public enum DistrictType {
-
-    ASSEMBLY, CONGRESSIONAL, COUNTY, ELECTION, SENATE, SCHOOL, TOWN
-
-}
