f2d0650928ef6979c9031d15845f7a3d807dee2c
Ash Islam <ashislam858@gmail.com>
2013-06-05 12:55:45 -0400

    AdminView startup code

---
+++ src/main/java/gov/nysenate/sage/model/admin/ApiUserStats.java
@@ -0,0 +1,20 @@
+package gov.nysenate.sage.model.admin;
+
+import gov.nysenate.sage.model.api.ApiUser;
+
+/**
+ *
+ */
+public class ApiUserStats
+{
+    private ApiUser apiUser;
+
+
+    public ApiUser getApiUser() {
+        return apiUser;
+    }
+
+    public void setApiUser(ApiUser apiUser) {
+        this.apiUser = apiUser;
+    }
+}
