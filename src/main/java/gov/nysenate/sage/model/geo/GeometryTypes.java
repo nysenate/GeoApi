87a66b95f900cfb624f0cb151a638f34e892e456
Ash Islam <ashislam858@gmail.com>
2013-07-08 01:40:20 -0400

    added monitoring dependency

---
+++ src/main/java/gov/nysenate/sage/model/geo/GeometryTypes.java
@@ -0,0 +1,11 @@
+package gov.nysenate.sage.model.geo;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: ash
+ * Date: 7/3/13
+ * Time: 1:25 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class GeometryTypes {
+}
