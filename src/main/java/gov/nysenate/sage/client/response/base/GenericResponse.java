7faa27ec5ddbc84be1aeb16d8dd818e259452f5e
Ash Islam <ashislam858@gmail.com>
2013-07-16 12:50:02 -0400

    Added send job email option in app.example.properties

---
+++ src/main/java/gov/nysenate/sage/client/response/base/GenericResponse.java
@@ -0,0 +1,11 @@
+package gov.nysenate.sage.client.response.base;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: ash
+ * Date: 7/15/13
+ * Time: 3:46 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class GenericResponse {
+}
---
+++ src/main/java/gov/nysenate/sage/model/stats/ApiLogLevel.java
@@ -0,0 +1,11 @@
+package gov.nysenate.sage.model.stats;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: ash
+ * Date: 7/15/13
+ * Time: 11:53 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public enum ApiLogLevel {
+}
