9c3a2072799dc04d008f39ce1a9ad45415a404c6
Anthony Calabrese <aacal050@gmail.com>
2018-05-31 10:44:30 -0400

    Convert Address controller to spring
    I am working on converting the api controllers to spring controllers.
    Hopefully when it is all done, no functionality will be lost, and
    anything calling sage won't know the difference between Sage-spring and
    the current sage-prod

--- src/main/java/gov/nysenate/sage/controller/api/AddressController.java
+++ src/main/java/gov/nysenate/sage/controller/api/AddressController.java
@@ -6,11 +6,15 @@ import gov.nysenate.sage.model.address.Address;
 import gov.nysenate.sage.model.api.ApiRequest;
 import gov.nysenate.sage.service.address.AddressService;
 import gov.nysenate.sage.service.address.AddressServiceProvider;
+import gov.nysenate.sage.util.controller.ConstantUtil;
 import org.apache.commons.io.IOUtils;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
@@ -19,7 +23,11 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.ArrayList;
 
+import static gov.nysenate.sage.filter.ApiFilter.getApiRequest;
 import static gov.nysenate.sage.model.result.ResultStatus.*;
+import static gov.nysenate.sage.util.controller.ApiControllerUtil.getAddressFromParams;
+import static gov.nysenate.sage.util.controller.ApiControllerUtil.getAddressesFromJsonBody;
+import static gov.nysenate.sage.util.controller.ApiControllerUtil.setApiResponse;
 
 /**
  * Address API controller handles the various AddressService requests including
@@ -28,7 +36,8 @@ import static gov.nysenate.sage.model.result.ResultStatus.*;
  *  - ZipCode Lookup
  */
 @Controller
-public final class AddressController extends BaseApiController
+@RequestMapping(value = ConstantUtil.REST_PATH + "address")
+public final class AddressController
 {
     private Logger logger = LogManager.getLogger(AddressController.class);
     private static AddressServiceProvider addressProvider;
@@ -38,30 +47,112 @@ public final class AddressController extends BaseApiController
         this.addressProvider = addressProvider;
     }
 
-    @Override
-    public void init(ServletConfig config) throws ServletException
-    {
-        logger.debug("Initialized " + this.getClass().getSimpleName());
-    }
-
-    /** Proxies to doGet() */
-    @Override
-    public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
-    {
-        this.doGet(request, response);
-    }
-
-    @Override
-    public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
-    {
-        Object addressResponse;
+    @RequestMapping(value = "/validate", method = RequestMethod.GET)
+    public void addressValidate(HttpServletRequest request, HttpServletResponse response,
+                                @RequestParam String provider, @RequestParam boolean punct,
+                                @RequestParam String addr, @RequestParam String addr1, @RequestParam String addr2,
+                                @RequestParam String city, @RequestParam String state, @RequestParam String zip5,
+                                @RequestParam String zip4) {
+        Object addressResponse = new ApiError(this.getClass(), PROVIDER_NOT_SUPPORTED);
 
         /** Get the ApiRequest */
         ApiRequest apiRequest = getApiRequest(request);
-        String provider = apiRequest.getProvider();
 
-        Boolean usePunctuation = Boolean.parseBoolean(request.getParameter("punct"));
+        logAddressInput(apiRequest, request, punct);
 
+        if (checkProvider(provider, addressResponse)) {
+            Address address = getAddressFromParams(addr,addr1,addr2,city,state,zip5,zip4);
+            addressResponse = new ValidateResponse(addressProvider.validate(address, provider, punct));
+        }
+        setApiResponse(addressResponse, request);
+
+    }
+
+    @RequestMapping(value = "/citystate", method = RequestMethod.GET)
+    public void addressCityState(HttpServletRequest request, HttpServletResponse response,
+                                 @RequestParam String provider, @RequestParam boolean punct,
+                                 @RequestParam String addr, @RequestParam String addr1, @RequestParam String addr2,
+                                 @RequestParam String city, @RequestParam String state, @RequestParam String zip5,
+                                 @RequestParam String zip4) {
+        Object addressResponse = new ApiError(this.getClass(), PROVIDER_NOT_SUPPORTED);
+
+        /** Get the ApiRequest */
+        ApiRequest apiRequest = getApiRequest(request);
+        logAddressInput(apiRequest, request, punct);
+
+        if (checkProvider(provider, addressResponse)) {
+            Address address = getAddressFromParams(addr,addr1,addr2,city,state,zip5,zip4);
+            addressResponse = new CityStateResponse(addressProvider.lookupCityState(address, provider));
+        }
+        setApiResponse(addressResponse, request);
+    }
+
+    @RequestMapping(value = "/zipcode", method = RequestMethod.GET)
+    public void addressZipcode(HttpServletRequest request, HttpServletResponse response,
+                               @RequestParam String provider, @RequestParam boolean punct,
+                               @RequestParam String addr, @RequestParam String addr1, @RequestParam String addr2,
+                               @RequestParam String city, @RequestParam String state, @RequestParam String zip5,
+                               @RequestParam String zip4) {
+        Object addressResponse = new ApiError(this.getClass(), PROVIDER_NOT_SUPPORTED);
+
+        /** Get the ApiRequest */
+        ApiRequest apiRequest = getApiRequest(request);
+        logAddressInput(apiRequest, request, punct);
+
+        if (checkProvider(provider, addressResponse)) {
+            Address address = getAddressFromParams(addr,addr1,addr2,city,state,zip5,zip4);
+            addressResponse = new ZipcodeResponse(addressProvider.lookupZipcode(address, provider));
+        }
+        setApiResponse(addressResponse, request);
+    }
+
+
+    @RequestMapping(value = "/batch/validate", method = RequestMethod.GET)
+    public void addressBatchValidate(HttpServletRequest request, HttpServletResponse response,
+                                     @RequestParam String provider, @RequestParam boolean punct) throws IOException {
+        /** Get the ApiRequest */
+        ApiRequest apiRequest = getApiRequest(request);
+        logAddressInput(apiRequest, request, punct);
+
+        Object addressResponse = new ApiError(this.getClass(), INVALID_BATCH_ADDRESSES);
+
+        String batchJsonPayload = IOUtils.toString(request.getInputStream(), "UTF-8");
+        ArrayList<Address> addresses = getAddressesFromJsonBody(batchJsonPayload);
+
+        if (checkProvider(provider, addressResponse)) {
+            if (addresses != null && !addresses.isEmpty()) {
+                AddressService addressService = addressProvider.newInstance(provider, true);
+                addressResponse = new BatchValidateResponse(addressService.validate(addresses));
+            }
+        }
+    }
+
+    @RequestMapping(value = "/batch/citystate", method = RequestMethod.GET)
+    public void addressBatchCityState(HttpServletRequest request, HttpServletResponse response,
+                                      @RequestParam String provider, @RequestParam boolean punct) throws IOException {
+        /** Get the ApiRequest */
+        ApiRequest apiRequest = getApiRequest(request);
+        logAddressInput(apiRequest, request, punct);
+
+        Object addressResponse = new ApiError(this.getClass(), INVALID_BATCH_ADDRESSES);
+
+        String batchJsonPayload = IOUtils.toString(request.getInputStream(), "UTF-8");
+        ArrayList<Address> addresses = getAddressesFromJsonBody(batchJsonPayload);
+
+        if (checkProvider(provider, addressResponse)) {
+            if (addresses != null && !addresses.isEmpty()) {
+                AddressService addressService = addressProvider.newInstance(provider, true);
+                addressResponse = new BatchCityStateResponse(addressService.lookupCityState(addresses));
+            }
+        }
+
+        setApiResponse(addressResponse, request);
+
+
+    }
+
+
+    private void logAddressInput(ApiRequest apiRequest, HttpServletRequest request,boolean usePunctuation ) {
         logger.info("--------------------------------------");
         logger.info(String.format("|%sAddress Request %d ", (apiRequest.isBatch() ? " Batch " : " "), apiRequest.getId()));
         logger.info(String.format("| Mode: %s | Punct: %s", apiRequest.getRequest(), usePunctuation));
@@ -69,72 +160,15 @@ public final class AddressController extends BaseApiController
             logger.info("| Input Address: " + getAddressFromParams(request).toLogString());
         }
         logger.info("--------------------------------------");
+    }
 
-        /**
-         * If provider is specified then make sure it matches the available providers. Send an
-         * api error and return if the provider is not supported.
-         */
+    private boolean checkProvider(String provider, Object addressResponse) {
+        boolean providerIsGood = true;
         if (provider != null && !provider.isEmpty()) {
             if (!addressProvider.isRegistered(provider)) {
-                addressResponse = new ApiError(this.getClass(), PROVIDER_NOT_SUPPORTED);
-                setApiResponse(addressResponse, request);
-                return;
+                providerIsGood = false;
             }
         }
-
-        /** Handle single request */
-        if (!apiRequest.isBatch()) {
-            Address address = getAddressFromParams(request);
-            if (address != null && !address.isEmpty()) {
-                switch (apiRequest.getRequest()) {
-                    case "validate": {
-                        addressResponse = new ValidateResponse(addressProvider.validate(address, provider, usePunctuation));
-                        break;
-                    }
-                    case "citystate" : {
-                        addressResponse = new CityStateResponse(addressProvider.lookupCityState(address, provider));
-                        break;
-                    }
-                    case "zipcode" : {
-                        addressResponse = new ZipcodeResponse(addressProvider.lookupZipcode(address, provider));
-                        break;
-                    }
-                    default: {
-                        addressResponse = new ApiError(this.getClass(), SERVICE_NOT_SUPPORTED);
-                    }
-                }
-            }
-            else {
-                addressResponse = new ApiError(this.getClass(), MISSING_ADDRESS);
-            }
-        }
-        /** Handle batch request */
-        else {
-            String batchJsonPayload = IOUtils.toString(request.getInputStream(), "UTF-8");
-            ArrayList<Address> addresses = getAddressesFromJsonBody(batchJsonPayload);
-            if (addresses != null && !addresses.isEmpty()) {
-                switch (apiRequest.getRequest()) {
-                    case "validate": {
-                        AddressService addressService = addressProvider.newInstance(provider, true);
-                        addressResponse = new BatchValidateResponse(addressService.validate(addresses));
-                        break;
-                    }
-                    case "citystate": {
-                        AddressService addressService = addressProvider.newInstance(provider, true);
-                        addressResponse = new BatchCityStateResponse(addressService.lookupCityState(addresses));
-                        break;
-                    }
-                    default : {
-                        addressResponse = new ApiError(this.getClass(), SERVICE_NOT_SUPPORTED);
-                    }
-                }
-            }
-            else {
-                addressResponse = new ApiError(this.getClass(), INVALID_BATCH_ADDRESSES);
-            }
-        }
-
-        /** Set response */
-        setApiResponse(addressResponse, request);
+        return providerIsGood;
     }
 }
