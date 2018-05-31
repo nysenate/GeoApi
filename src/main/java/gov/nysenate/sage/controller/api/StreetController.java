1b3a7c7e1890c3c3c133f7ed15783d901cbd78c9
Anthony Calabrese <aacal050@gmail.com>
2018-05-31 11:33:45 -0400

    Convert Street Controller to Spring
    the Street controller no longer relies on http servlet and is now
    managed by spring

--- src/main/java/gov/nysenate/sage/controller/api/StreetController.java
+++ src/main/java/gov/nysenate/sage/controller/api/StreetController.java
@@ -8,10 +8,14 @@ import gov.nysenate.sage.model.api.ApiRequest;
 import gov.nysenate.sage.model.result.ResultStatus;
 import gov.nysenate.sage.model.result.StreetResult;
 import gov.nysenate.sage.service.street.StreetLookupServiceProvider;
+import gov.nysenate.sage.util.controller.ConstantUtil;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
@@ -20,70 +24,57 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.List;
 
+import static gov.nysenate.sage.filter.ApiFilter.getApiRequest;
+import static gov.nysenate.sage.util.controller.ApiControllerUtil.setApiResponse;
+
 @Controller
-public class StreetController extends BaseApiController
+@RequestMapping(value = ConstantUtil.REST_PATH + "street")
+public class StreetController
 {
     private static Logger logger = LogManager.getLogger(StreetController.class);
     private static StreetLookupServiceProvider streetProvider;
 
     @Autowired
-    public StreetController(StreetLookupServiceProvider streetProvider, Environment env) {
+    public StreetController(StreetLookupServiceProvider streetProvider) {
         this.streetProvider = streetProvider;
     }
 
-    @Override
-    public void init(ServletConfig config) throws ServletException {}
-
-    @Override
-    public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
-    {
-        doGet(request, response);
-    }
-
-    @Override
-    public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
-    {
+    @RequestMapping(value = "/lookup", method = RequestMethod.GET)
+    public void addressBatchCityState(HttpServletRequest request, HttpServletResponse response,
+                                      @RequestParam String zip5) throws IOException {
         Object streetLookupResponse;
-
         /** Get the ApiRequest */
         ApiRequest apiRequest = getApiRequest(request);
+        logStreetRequest(apiRequest,zip5);
 
-        /** Zip5 is required to perform street lookup */
-        String zip5 = request.getParameter("zip5");
+        if (zip5 != null && !zip5.isEmpty()) {
+            logger.info("Getting street data for zip5 " + zip5);
+            StreetResult streetResult = new StreetResult(this.getClass());
+            List<DistrictedStreetRange> streets = streetProvider.getInstance().streetLookup(zip5);
+            if (streets != null) {
+                streetResult.setDistrictedStreetRanges(streets);
+                streetResult.setStatusCode(ResultStatus.SUCCESS);
+                logger.info("Street file look up for zip 5: "+ zip5 + " was successful");
+            }
+            else {
+                streetResult.setStatusCode(ResultStatus.NO_STREET_LOOKUP_RESULT);
+                logger.warn("No street lookup result was found from the request zip 5:" + zip5);
+            }
+            streetLookupResponse = new StreetResponse(streetResult);
+        }
+        else {
+            streetLookupResponse = new ApiError(this.getClass(), ResultStatus.MISSING_ZIPCODE);
+            logger.warn("The api request is missing a zip 5 code");
+        }
 
+        setApiResponse(streetLookupResponse, request);
+    }
+
+
+    private void logStreetRequest(ApiRequest apiRequest, String zip5) {
         logger.info("--------------------------------------");
         logger.info(String.format("|%s Street Request %d ", (apiRequest.isBatch() ? " Batch " : " "), apiRequest.getId()));
         logger.info(String.format("| Mode: %s | zip5: %s", apiRequest.getRequest(), zip5));
         logger.info("--------------------------------------");
-
-        switch (apiRequest.getRequest()) {
-            case "lookup" :
-                if (zip5 != null && !zip5.isEmpty()) {
-                    logger.info("Getting street data for zip5 " + zip5);
-                    StreetResult streetResult = new StreetResult(this.getClass());
-                    List<DistrictedStreetRange> streets = streetProvider.getInstance().streetLookup(zip5);
-                    if (streets != null) {
-                        streetResult.setDistrictedStreetRanges(streets);
-                        streetResult.setStatusCode(ResultStatus.SUCCESS);
-                        logger.info("Street file look up for zip 5: "+ zip5 + " was successful");
-                    }
-                    else {
-                        streetResult.setStatusCode(ResultStatus.NO_STREET_LOOKUP_RESULT);
-                        logger.warn("No street lookup result was found from the request zip 5:" + zip5);
-                    }
-                    streetLookupResponse = new StreetResponse(streetResult);
-                }
-                else {
-                    streetLookupResponse = new ApiError(this.getClass(), ResultStatus.MISSING_ZIPCODE);
-                    logger.warn("The api request is missing a zip 5 code");
-                }
-                break;
-
-            default :
-                streetLookupResponse = new ApiError(this.getClass(), ResultStatus.SERVICE_NOT_SUPPORTED);
-                logger.warn("The api requested service is not supported");
-        }
-
-        setApiResponse(streetLookupResponse, request);
     }
 }
