32b64f0777e8db0bc42883609c4615399a24992c
Anthony Calabrese <aacal050@gmail.com>
2018-05-31 15:41:49 -0400

    Convert Geocode Controller to spring
    The geocode controller is no longer dependent on an http servlet
    implementation. Instead, this controller now relies on Spring
    management

--- src/main/java/gov/nysenate/sage/controller/api/GeocodeController.java
+++ src/main/java/gov/nysenate/sage/controller/api/GeocodeController.java
@@ -16,27 +16,29 @@ import gov.nysenate.sage.model.result.GeocodeResult;
 import gov.nysenate.sage.service.geo.GeocodeServiceProvider;
 import gov.nysenate.sage.service.geo.RevGeocodeServiceProvider;
 import gov.nysenate.sage.util.TimeUtil;
+import gov.nysenate.sage.util.controller.ConstantUtil;
 import org.apache.commons.io.IOUtils;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-
-import javax.servlet.ServletConfig;
-import javax.servlet.ServletException;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.sql.Timestamp;
 import java.util.List;
-import java.util.Observable;
-import java.util.Observer;
 
+import static gov.nysenate.sage.filter.ApiFilter.getApiRequest;
 import static gov.nysenate.sage.model.result.ResultStatus.*;
+import static gov.nysenate.sage.util.controller.ApiControllerUtil.*;
 
 /** Handles Geo Api requests */
 @Controller
-public class GeocodeController extends BaseApiController implements Observer
+@RequestMapping(value = ConstantUtil.REST_PATH + "geo")
+public class GeocodeController
 {
     private static Logger logger = LogManager.getLogger(GeocodeController.class);
     private static GeocodeServiceProvider geocodeServiceProvider;
@@ -58,62 +60,202 @@ public class GeocodeController extends BaseApiController implements Observer
         this.geocodeResultLogger = geocodeResultLogger;
         this.geocodeServiceProvider = geocodeServiceProvider;
         this.revGeocodeServiceProvider = revGeocodeServiceProvider;
-    }
 
-    @Override
-    public void init(ServletConfig config) throws ServletException
-    {
         logger.debug("Initialized " + this.getClass().getSimpleName());
-        update(null, null);
-    }
-
-    @Override
-    public void update(Observable o, Object arg) {
         boolean API_LOGGING_ENABLED = env.isApiLoggingEnabled();
         SINGLE_LOGGING_ENABLED = API_LOGGING_ENABLED && env.isDetailedLoggingEnabled();
         BATCH_LOGGING_ENABLED = API_LOGGING_ENABLED && env.isBatchDetailedLoggingEnabled();
     }
 
-    @Override
-    public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
-    {
-        doGet(request, response);
-    }
+    @RequestMapping(value = "/geocode", method = RequestMethod.GET)
+    public void geoGeocode(HttpServletRequest request, HttpServletResponse response,
+                                @RequestParam String provider,
+                                @RequestParam String addr, @RequestParam String addr1, @RequestParam String addr2,
+                                @RequestParam String city, @RequestParam String state, @RequestParam String zip5,
+                                @RequestParam String zip4, @RequestParam boolean useFallback,
+                                @RequestParam boolean doNotCache, @RequestParam boolean bypassCache) {
 
-    @Override
-    public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
-    {
         Object geocodeResponse = new ApiError(this.getClass(), SERVICE_NOT_SUPPORTED);
-        Timestamp startTime = TimeUtil.currentTimestamp();
-
-        /** Get the ApiRequest */
+        Timestamp startTime = getCurrentTimeStamp();
         ApiRequest apiRequest = getApiRequest(request);
-        String provider = apiRequest.getProvider();
+        Boolean useCache = true;
 
-        /** Check whether or not to fallback */
-        boolean useFallback = requestParameterEquals(request, "useFallback", "false") ? false : true;
-
-        boolean bypassCache = requestParameterEquals(request, "bypassCache", "false") ? false : true;
-
-        boolean doNotCache = requestParameterEquals(request, "doNotCache", "false") ? true : false;
-
-
-        /** Only want to use cache when the provider is not specified */
-        boolean useCache = (provider == null);
-        if (bypassCache || doNotCache) {
-            useCache = false;
-        }
-        if (provider != null && provider.equals("geocache")) {
-            useCache = true;
-        }
-
+        determineCacheProviderProps(useCache,bypassCache,doNotCache,provider);
 
         int requestId = -1;
 
         /** Construct a GeocodeRequest using the supplied params */
-        GeocodeRequest geocodeRequest = new GeocodeRequest(apiRequest, getAddressFromParams(request), provider, useFallback, useCache, bypassCache, doNotCache);
+        GeocodeRequest geocodeRequest = new GeocodeRequest(apiRequest,
+                getAddressFromParams(addr,addr1,addr2,city,state,zip5,zip4),
+                provider, useFallback, useCache, bypassCache, doNotCache);
 
+        logGeoRequest(apiRequest,geocodeRequest,requestId);
 
+        if (!checkProvider(provider, geocodeResponse, request)) {
+            return;
+        }
+
+        if (geocodeRequest.getAddress() != null && !geocodeRequest.getAddress().isEmpty()) {
+            /** Obtain geocode result */
+            GeocodeResult geocodeResult = geocodeServiceProvider.geocode(geocodeRequest);
+
+            /** Construct response from request */
+            geocodeResponse = new GeocodeResponse(geocodeResult);
+
+            /** Log geocode request/result to database */
+            if (SINGLE_LOGGING_ENABLED && requestId != -1) {
+                geocodeResultLogger.logGeocodeResult(requestId, geocodeResult);
+            }
+        }
+        else {
+            geocodeResponse = new ApiError(this.getClass(), MISSING_ADDRESS);
+        }
+
+        logElaspedTime(startTime);
+        setApiResponse(geocodeResponse,request);
+    }
+
+    @RequestMapping(value = "/revgeocode", method = RequestMethod.GET)
+    public void geoRevGeocode(HttpServletRequest request, HttpServletResponse response,
+                           @RequestParam String provider, @RequestParam String lat, @RequestParam String lon,
+                              @RequestParam boolean useFallback, @RequestParam boolean doNotCache,
+                              @RequestParam boolean bypassCache) {
+        Object geocodeResponse = new ApiError(this.getClass(), SERVICE_NOT_SUPPORTED);
+        Timestamp startTime = getCurrentTimeStamp();
+        ApiRequest apiRequest = getApiRequest(request);
+        Boolean useCache = true;
+
+        determineCacheProviderProps(useCache,bypassCache,doNotCache,provider);
+
+        int requestId = -1;
+
+        /** Construct a GeocodeRequest using the supplied params */
+        GeocodeRequest geocodeRequest = new GeocodeRequest(apiRequest,
+                new Address(), provider, useFallback, useCache, bypassCache, doNotCache);
+
+        logGeoRequest(apiRequest,geocodeRequest,requestId);
+
+        if (!checkProvider(provider, geocodeResponse, request)) {
+            return;
+        }
+
+        Point point = getPointFromParams(request);
+        if (point != null ) {
+            geocodeRequest.setReverse(true);
+            geocodeRequest.setPoint(point);
+            GeocodeResult revGeocodeResult = revGeocodeServiceProvider.reverseGeocode(geocodeRequest);
+            geocodeResponse = new RevGeocodeResponse(revGeocodeResult);
+
+            /** Log rev geocode request/result to database */
+            if (SINGLE_LOGGING_ENABLED) {
+                geocodeResultLogger.logGeocodeResult(requestId, revGeocodeResult);
+                requestId = -1;
+            }
+        }
+        else {
+            geocodeResponse = new ApiError(this.getClass(), MISSING_POINT);
+        }
+
+        logElaspedTime(startTime);
+        setApiResponse(geocodeResponse,request);
+    }
+
+    @RequestMapping(value = "/batch/geocode", method = RequestMethod.GET)
+    public void geoBatchGeocode(HttpServletRequest request, HttpServletResponse response,
+                           @RequestParam String provider, @RequestParam boolean useFallback,
+                           @RequestParam boolean doNotCache, @RequestParam boolean bypassCache) throws IOException {
+
+        Object geocodeResponse = new ApiError(this.getClass(), SERVICE_NOT_SUPPORTED);
+        Timestamp startTime = getCurrentTimeStamp();
+        ApiRequest apiRequest = getApiRequest(request);
+        Boolean useCache = true;
+
+        determineCacheProviderProps(useCache,bypassCache,doNotCache,provider);
+
+        int requestId = -1;
+
+        /** Construct a GeocodeRequest using the supplied params */
+        GeocodeRequest geocodeRequest = new GeocodeRequest(apiRequest,
+                new Address(), provider, useFallback, useCache, bypassCache, doNotCache);
+
+        logGeoRequest(apiRequest,geocodeRequest,requestId);
+
+        if (!checkProvider(provider, geocodeResponse, request)) {
+            return;
+        }
+
+        String batchJsonPayload = IOUtils.toString(request.getInputStream(), "UTF-8");
+        List<Address> addresses = getAddressesFromJsonBody(batchJsonPayload);
+        if (addresses.size() > 0) {
+            BatchGeocodeRequest batchGeocodeRequest = new BatchGeocodeRequest(geocodeRequest);
+            batchGeocodeRequest.setAddresses(addresses);
+
+            List<GeocodeResult> geocodeResults = geocodeServiceProvider.geocode(batchGeocodeRequest);
+            geocodeResponse = new BatchGeocodeResponse(geocodeResults);
+
+            if (BATCH_LOGGING_ENABLED) {
+                geocodeResultLogger.logBatchGeocodeResults(batchGeocodeRequest, geocodeResults, true);
+            }
+        }
+        else {
+            geocodeResponse = new ApiError(this.getClass(), INVALID_BATCH_ADDRESSES);
+        }
+
+        logElaspedTime(startTime);
+        setApiResponse(geocodeResponse,request);
+    }
+
+    @RequestMapping(value = "/batch/revgeocode", method = RequestMethod.GET)
+    public void geoBatchRevGeocode(HttpServletRequest request, HttpServletResponse response,
+                           @RequestParam String provider,
+                           @RequestParam String addr, @RequestParam String addr1, @RequestParam String addr2,
+                           @RequestParam String city, @RequestParam String state, @RequestParam String zip5,
+                           @RequestParam String zip4, @RequestParam boolean useFallback,
+                           @RequestParam boolean doNotCache, @RequestParam boolean bypassCache) throws IOException {
+
+        Object geocodeResponse = new ApiError(this.getClass(), SERVICE_NOT_SUPPORTED);
+        Timestamp startTime = getCurrentTimeStamp();
+        ApiRequest apiRequest = getApiRequest(request);
+        Boolean useCache = true;
+
+        determineCacheProviderProps(useCache,bypassCache,doNotCache,provider);
+
+        int requestId = -1;
+
+        /** Construct a GeocodeRequest using the supplied params */
+        GeocodeRequest geocodeRequest = new GeocodeRequest(apiRequest,
+                new Address(), provider, useFallback, useCache, bypassCache, doNotCache);
+
+        logGeoRequest(apiRequest,geocodeRequest,requestId);
+
+        if (!checkProvider(provider, geocodeResponse, request)) {
+            return;
+        }
+
+        String batchJsonPayload = IOUtils.toString(request.getInputStream(), "UTF-8");
+        List<Point> points = getPointsFromJsonBody(batchJsonPayload);
+        if (points.size() > 0) {
+            BatchGeocodeRequest batchGeocodeRequest = new BatchGeocodeRequest(geocodeRequest);
+            batchGeocodeRequest.setReverse(true);
+            batchGeocodeRequest.setPoints(points);
+
+            List<GeocodeResult> revGeocodeResults = revGeocodeServiceProvider.reverseGeocode(batchGeocodeRequest);
+            geocodeResponse = new BatchGeocodeResponse(revGeocodeResults);
+
+            if (BATCH_LOGGING_ENABLED) {
+                geocodeResultLogger.logBatchGeocodeResults(batchGeocodeRequest, revGeocodeResults, true);
+            }
+        }
+        else {
+            geocodeResponse = new ApiError(this.getClass(), INVALID_BATCH_POINTS);
+        }
+
+        logElaspedTime(startTime);
+        setApiResponse(geocodeResponse,request);
+
+    }
+
+    private void logGeoRequest(ApiRequest apiRequest, GeocodeRequest geocodeRequest, int requestId) {
         logger.info("=======================================================");
         logger.info(String.format("|%sGeocode Request %d ", (apiRequest.isBatch() ? " Batch " : " "), apiRequest.getId()));
         logger.info(String.format("| Mode: %s | IP: %s | Provider: %s", apiRequest.getRequest(), apiRequest.getIpAddress(), apiRequest.getProvider()));
@@ -125,7 +267,9 @@ public class GeocodeController extends BaseApiController implements Observer
         if (SINGLE_LOGGING_ENABLED) {
             requestId = geocodeRequestLogger.logGeocodeRequest(geocodeRequest);
         }
+    }
 
+    private boolean checkProvider(String provider, Object geocodeResponse, HttpServletRequest request) {
         /**
          * If provider is specified then make sure it matches the available providers. Send an
          * api error and return if the provider is not supported.
@@ -133,100 +277,28 @@ public class GeocodeController extends BaseApiController implements Observer
         if (provider != null && !provider.isEmpty() && !geocodeServiceProvider.isRegistered(provider) && !provider.equals("geocache")) {
             geocodeResponse = new ApiError(this.getClass(), PROVIDER_NOT_SUPPORTED);
             setApiResponse(geocodeResponse, request);
-            return;
+            return false;
         }
-
-        switch (apiRequest.getRequest()) {
-            case "geocode":
-            {
-                /** Handle single geocoding requests */
-                if (!apiRequest.isBatch()) {
-                    if (geocodeRequest.getAddress() != null && !geocodeRequest.getAddress().isEmpty()) {
-                        /** Obtain geocode result */
-                        GeocodeResult geocodeResult = geocodeServiceProvider.geocode(geocodeRequest);
-
-                        /** Construct response from request */
-                        geocodeResponse = new GeocodeResponse(geocodeResult);
-
-                        /** Log geocode request/result to database */
-                        if (SINGLE_LOGGING_ENABLED && requestId != -1) {
-                            geocodeResultLogger.logGeocodeResult(requestId, geocodeResult);
-                            requestId = -1;
-                        }
-                    }
-                    else {
-                        geocodeResponse = new ApiError(this.getClass(), MISSING_ADDRESS);
-                    }
-                }
-                /** Handle batch geocoding requests */
-                else {
-                    String batchJsonPayload = IOUtils.toString(request.getInputStream(), "UTF-8");
-                    List<Address> addresses = getAddressesFromJsonBody(batchJsonPayload);
-                    if (addresses.size() > 0) {
-                        BatchGeocodeRequest batchGeocodeRequest = new BatchGeocodeRequest(geocodeRequest);
-                        batchGeocodeRequest.setAddresses(addresses);
-
-                        List<GeocodeResult> geocodeResults = geocodeServiceProvider.geocode(batchGeocodeRequest);
-                        geocodeResponse = new BatchGeocodeResponse(geocodeResults);
-
-                        if (BATCH_LOGGING_ENABLED) {
-                            geocodeResultLogger.logBatchGeocodeResults(batchGeocodeRequest, geocodeResults, true);
-                        }
-                    }
-                    else {
-                        geocodeResponse = new ApiError(this.getClass(), INVALID_BATCH_ADDRESSES);
-                    }
-                }
-                break;
-            }
-            case "revgeocode" :
-            {
-                /** Handle single rev geocoding requests */
-                if (!apiRequest.isBatch()) {
-                    Point point = getPointFromParams(request);
-                    if (point != null ) {
-                        geocodeRequest.setReverse(true);
-                        geocodeRequest.setPoint(point);
-                        GeocodeResult revGeocodeResult = revGeocodeServiceProvider.reverseGeocode(geocodeRequest);
-                        geocodeResponse = new RevGeocodeResponse(revGeocodeResult);
-
-                        /** Log rev geocode request/result to database */
-                        if (SINGLE_LOGGING_ENABLED) {
-                            geocodeResultLogger.logGeocodeResult(requestId, revGeocodeResult);
-                            requestId = -1;
-                        }
-                    }
-                    else {
-                        geocodeResponse = new ApiError(this.getClass(), MISSING_POINT);
-                    }
-                }
-                /** Handle batch rev geocoding requests */
-                else {
-                    String batchJsonPayload = IOUtils.toString(request.getInputStream(), "UTF-8");
-                    List<Point> points = getPointsFromJsonBody(batchJsonPayload);
-                    if (points.size() > 0) {
-                        BatchGeocodeRequest batchGeocodeRequest = new BatchGeocodeRequest(geocodeRequest);
-                        batchGeocodeRequest.setReverse(true);
-                        batchGeocodeRequest.setPoints(points);
-
-                        List<GeocodeResult> revGeocodeResults = revGeocodeServiceProvider.reverseGeocode(batchGeocodeRequest);
-                        geocodeResponse = new BatchGeocodeResponse(revGeocodeResults);
-
-                        if (BATCH_LOGGING_ENABLED) {
-                            geocodeResultLogger.logBatchGeocodeResults(batchGeocodeRequest, revGeocodeResults, true);
-                        }
-                    }
-                    else {
-                        geocodeResponse = new ApiError(this.getClass(), INVALID_BATCH_POINTS);
-                    }
-                }
-                break;
-            }
-        }
-
-        logger.info(String.format("Geo Response in %s ms.", TimeUtil.getElapsedMs(startTime)));
-
-        /** Set response */
-        setApiResponse(geocodeResponse, request);
+        return true;
     }
+
+    private void determineCacheProviderProps(boolean useCache, boolean bypassCache, boolean doNotCache, String provider) {
+        /** Only want to use cache when the provider is not specified */
+        useCache = (provider == null);
+        if (bypassCache || doNotCache) {
+            useCache = false;
+        }
+        if (provider != null && provider.equals("geocache")) {
+            useCache = true;
+        }
+    }
+
+    private void logElaspedTime(Timestamp startTime) {
+        logger.info(String.format("Geo Response in %s ms.", TimeUtil.getElapsedMs(startTime)));
+    }
+
+    private Timestamp getCurrentTimeStamp() {
+        return TimeUtil.currentTimestamp();
+    }
+
 }
