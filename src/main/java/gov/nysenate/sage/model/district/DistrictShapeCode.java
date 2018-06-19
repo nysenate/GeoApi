78a9ca0088f0203f2b0de2ef3dfb1d24adaa4507
Anthony Calabrese <aacal050@gmail.com>
2018-06-19 17:28:12 -0400

    Save new DistrictShapeCode Column headers
    After spending time researching how to update our DB with new street
    files, I changed the headers based off of what I observed in the Tiger
    Line 2017 files

--- src/main/java/gov/nysenate/sage/model/district/DistrictShapeCode.java
+++ src/main/java/gov/nysenate/sage/model/district/DistrictShapeCode.java
@@ -12,6 +12,8 @@ import java.util.HashMap;
  * use an external webservice like http://prj2epsg.org/search to convert into espg
  * code. From there you can use http://spatialreference.org/ref/epsg/{your espg code}
  * and get the srid using the PostGIS output.
+ * http://www.spatialreference.org/ref/epsg/4269/postgis/
+ * http://www.spatialreference.org/ref/epsg/26918/postgis/
  *
  * Alternatively, you can inspect the .prj file and the spatial_ref_sys table in the
  * geoapi database and try to find a match that way.
@@ -26,27 +28,27 @@ public enum DistrictShapeCode
 
     static {
         nameColumn.put(DistrictType.SENATE, "NAMELSAD");
-        nameColumn.put(DistrictType.CONGRESSIONAL, "NAME");
-        nameColumn.put(DistrictType.ASSEMBLY, "NAME");
+        nameColumn.put(DistrictType.CONGRESSIONAL, "NAMELSAD"); // NAME
+        nameColumn.put(DistrictType.ASSEMBLY, "NAMELSAD"); //NAME
         nameColumn.put(DistrictType.COUNTY, "NAMELSAD");
         nameColumn.put(DistrictType.SCHOOL, "NAME");
         nameColumn.put(DistrictType.TOWN, "NAME");
         nameColumn.put(DistrictType.ZIP, "ZCTA5CE10");
 
-        codeColumn.put(DistrictType.SENATE, "SD_CODE");
-        codeColumn.put(DistrictType.CONGRESSIONAL, "DISTRICT");
-        codeColumn.put(DistrictType.ASSEMBLY, "DISTRICT");
+        codeColumn.put(DistrictType.SENATE, "SLDUST"); //SD_CODE
+        codeColumn.put(DistrictType.CONGRESSIONAL, "CD115FP"); //DISTRICT
+        codeColumn.put(DistrictType.ASSEMBLY, "SLDLST"); //DISTRICT
         codeColumn.put(DistrictType.COUNTY, "COUNTYFP");
-        codeColumn.put(DistrictType.SCHOOL, "TFCODE");
+        codeColumn.put(DistrictType.SCHOOL, "UNSDLEA"); //TFCODE
         codeColumn.put(DistrictType.TOWN, "ABBREV");
         codeColumn.put(DistrictType.ZIP, "ZCTA5CE10");
 
         sridColumn.put(DistrictType.SENATE, "4269");
-        sridColumn.put(DistrictType.CONGRESSIONAL, "4326");
-        sridColumn.put(DistrictType.ASSEMBLY, "4326");
-        sridColumn.put(DistrictType.COUNTY, "4326");
-        sridColumn.put(DistrictType.SCHOOL, "4326");
-        sridColumn.put(DistrictType.TOWN, "4326");
+        sridColumn.put(DistrictType.CONGRESSIONAL, "4269"); //4326
+        sridColumn.put(DistrictType.ASSEMBLY, "4269"); //4326
+        sridColumn.put(DistrictType.COUNTY, "4269"); //4326
+        sridColumn.put(DistrictType.SCHOOL, "4269"); //4326
+        sridColumn.put(DistrictType.TOWN, "26918"); //4326
         sridColumn.put(DistrictType.ZIP, "4269");
     }
 
