fffee582112821c0a933a21bc1218fe4882673f7
Anthony Calabrese <aacal050@gmail.com>
2018-07-24 10:25:12 -0400

    Refactor DoBatchGeocache to handle Yahoo only
    When this script was originally created, it was with the intention that
    we would use google to regeocache everything in our geocache. After we
    found out about nys geocoder, it was not necessary to regeocache
    everything through google. Now we will only need to regeocache about
    112,000 or so that are unaccounted for in the conversion of their
    coordinates to our geocache

--- src/main/java/gov/nysenate/sage/scripts/DoBatchGeocache.java
+++ src/main/java/gov/nysenate/sage/scripts/DoBatchGeocache.java
@@ -4,7 +4,6 @@ import gov.nysenate.sage.factory.ApplicationFactory;
 import gov.nysenate.sage.model.address.Address;
 import gov.nysenate.sage.model.address.StreetAddress;
 import gov.nysenate.sage.util.Config;
-import gov.nysenate.sage.util.FormatUtil;
 import org.apache.commons.dbutils.QueryRunner;
 import org.apache.commons.dbutils.handlers.BeanListHandler;
 import org.apache.http.HttpResponse;
@@ -29,15 +28,17 @@ public class DoBatchGeocache {
     }
 
     public String createJsonForAddress(String addr1, String city, String state, String zip5) {
-        return "{\"addr1\": " + "\"" + addr1 + "\"" + "," + "\"city\": " + "\"" + city + "\"" + "," + "\"state\": " + "\"" + state + "\"" + "," + "\"zip5\": " + "\"" + zip5 + "\"" + "},";
+        return "{\"addr1\": " + "\"" + addr1 + "\"" + "," + "\"city\": " + "\"" + city + "\"" + "," +
+                "\"state\": " + "\"" + state + "\"" + "," + "\"zip5\": " + "\"" + zip5 + "\"" + "},";
     }
 
     public static void main(String[] args) {
         /*
-        Make sure that there were 2 arguments supplied to the script
+        Make sure that there were at least 2 arguments supplied to the script
          */
-        if (args.length != 2) {
-            System.err.println("Usage: DoBatchGeocache offset_number amount_to_geocode_number EX. DoBatchGeocache 0 100");
+        if (args.length < 2) {
+            System.err.println("Usage: DoBatchGeocache offset_number amount_to_geocode_number geocode_yahoo_only \n" +
+                    "EX. DoBatchGeocache 0 100 or DoBatchGeocache 0 100 true");
             System.exit(1);
         }
 
@@ -53,38 +54,60 @@ public class DoBatchGeocache {
         DoBatchGeocache doBatchGeocache = new DoBatchGeocache();
         long offset = 0;
         long limit = 10;
-        String BATCH_SQL = "select g.bldgnum, g.predir, g.street, g.streettype, g.postdir, g.location, g.state, g.zip5, g.zip4\n" +
-                "from cache.geocache g\n" +
-                "order by id asc\n" +
+        boolean yahoo = true;
+
+        String BATCH_SQL =
+                "select g.bldgnum, g.predir, g.street, g.streettype, g.postdir, g.location, g.state, g.zip5, g.zip4\n" +
+                "from cache.geocache g\n";
+
+        String OPTIONAL_WHERE_YAHOO =
+                "where method = 'YahooDao' and state = 'NY' and street != '' and streettype != '' and quality = 'HOUSE'\n";
+
+        String ORDER_SQL = "order by id asc\n" +
                 "limit ? OFFSET ?";
+
         BeanListHandler<StreetAddress> beanListHandler
                 = new BeanListHandler<>(StreetAddress.class);
 
         /*
         Create necessary variables to execute http request
          */
-        String httpRequestString = doBatchGeocache.config.getValue("base.url") + "/api/v2/geo/geocode/batch?provider=google&bypassCache=true&doNotCache=false&useFallback=false";
+        String httpRequestString = doBatchGeocache.config.getValue("base.url") +
+                "/api/v2/geo/geocode/batch?provider=google&bypassCache=true&doNotCache=false&useFallback=false";
         String httpRequestBody = "[";
 
         /*
-        Ensure that the arugments passed in are actually numbers
+        Ensure that the arugments passed in are actually numbers / determine if we geocode yahoo only
          */
         try {
             offset = Long.parseLong(args[0]);
             limit = Long.parseLong(args[1]);
+            if (args.length == 3) {
+                yahoo = Boolean.getBoolean(args[2]);
+            }
             logger.info("DoBatchGeocache input offset: " + offset + "\n" +
                     "DoBatchGeocache input limit: " + limit);
         }
         catch (Exception e) {
-            System.err.println("Supplied arguments were not integers");
+            System.err.println(
+                    "Check supplied arguments. The first 2 arguments should be intergers, the 3rd should be a boolean");
             System.exit(-1);
+        }
+
+        //Build SQL
+        if (yahoo) {
+            BATCH_SQL = BATCH_SQL + OPTIONAL_WHERE_YAHOO + ORDER_SQL;
+        }
+        else {
+            BATCH_SQL = BATCH_SQL + ORDER_SQL;
         }
 
         /*
         Execute SQL and form http request body
          */
         try {
-             List<StreetAddress> streetAddressesToGeocache = doBatchGeocache.tigerRun.query(BATCH_SQL, beanListHandler, limit, offset);
+             List<StreetAddress> streetAddressesToGeocache =
+                     doBatchGeocache.tigerRun.query(BATCH_SQL, beanListHandler, limit, offset);
              for (StreetAddress streetAddress: streetAddressesToGeocache) {
                  Address address = streetAddress.toAddress();
                  logger.debug(address.toString());
