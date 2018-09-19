baac72021bf463971d3dbfcaa90f979d35fc9717
Anthony Calabrese <aacal050@gmail.com>
2018-09-19 17:55:00 -0400

    Save groundwork for Inserting senate county codes
    The groundwork for extracting senate codes from the geoapi DB and
    inserting them into the tsv files based off of the file name is
    basically complete. These new scripts will need to be tested and then
    integrated into the master script.

---
+++ src/main/java/gov/nysenate/sage/scripts/StreetFinder/Scripts/GetSenateCountyCodes.java
@@ -0,0 +1,75 @@
+package gov.nysenate.sage.scripts.StreetFinder.Scripts;
+
+import gov.nysenate.sage.factory.ApplicationFactory;
+import gov.nysenate.sage.scripts.StreetFinder.County;
+import gov.nysenate.sage.util.Config;
+import org.apache.commons.dbutils.QueryRunner;
+import org.apache.commons.dbutils.ResultSetHandler;
+import org.apache.commons.dbutils.handlers.BeanListHandler;
+import org.apache.log4j.Logger;
+
+import java.io.*;
+import java.sql.SQLException;
+import java.util.List;
+
+public class GetSenateCountyCodes {
+
+    static Config config;
+    QueryRunner geoApiRun;
+    private static Logger logger = Logger.getLogger(GetTownCodes.class);
+
+    public GetSenateCountyCodes() {
+        config = ApplicationFactory.getConfig();
+        geoApiRun = new QueryRunner(ApplicationFactory.getDataSource());
+    }
+
+    public static void main(String[] args) {
+
+        /* Load up the configuration settings */
+        if (!ApplicationFactory.bootstrap()) {
+            System.err.println("Failed to configure application");
+            System.exit(-1);
+        }
+
+        GetSenateCountyCodes getSenateCountyCodes = new GetSenateCountyCodes();
+
+        String GET_SENATE_COUNTY_CODES = "select name, id from public.county";
+
+        String directory = "/data/geoapi_data/street_finder/senate_counties.txt";
+
+
+        try {
+            File senateCounties = new File(directory);
+            if (senateCounties.exists()) {
+                logger.info("File already exists");
+                System.exit(0);
+            }
+
+            senateCounties.createNewFile();
+
+            ResultSetHandler<List<County>> h = new BeanListHandler<>(County.class);
+
+            List<County> counties = getSenateCountyCodes.geoApiRun.query(GET_SENATE_COUNTY_CODES, h);
+
+            FileWriter fileWriter = new FileWriter(directory);
+            PrintWriter outputWriter = new PrintWriter(fileWriter);
+
+            int count = 0;
+            for (County county : counties) {
+                count++;
+                outputWriter.println(county.toString());
+            }
+
+            logger.info("Wrote " + count + " town codes to file");
+
+
+        } catch (SQLException ex) {
+            logger.error("Error retrieving town codes from geoapi db", ex);
+        } catch (IOException ex) {
+            logger.error("Error creating town code file", ex);
+        }
+
+
+    }
+
+}
