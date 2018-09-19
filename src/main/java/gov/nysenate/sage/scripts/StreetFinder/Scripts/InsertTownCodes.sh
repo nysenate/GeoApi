cbb70bd67f2cb0fbe51ae7140835e4313a367392
Anthony Calabrese <aacal050@gmail.com>
2018-09-19 17:54:11 -0400

    Update directory path for script
    The script was incorrectly looking at the wrong path for the war file of
    sage

--- src/main/java/gov/nysenate/sage/scripts/StreetFinder/Scripts/InsertTownCodes.sh
+++ src/main/java/gov/nysenate/sage/scripts/StreetFinder/Scripts/InsertTownCodes.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 source config
-java -classpath "${pathToGeoApi}/GeoApi/target/classes/:${pathToGeoApi}/GeoApi/target/geoapi##2.5/WEB-INF/lib/*" gov.nysenate.sage.scripts.StreetFinder.Scripts.InsertTownCode $1
+java -classpath "${pathToGeoApi}/GeoApi/target/classes/:${pathToGeoApi}/GeoApi/target/geoapi##2.5.1/WEB-INF/lib/*" gov.nysenate.sage.scripts.StreetFinder.Scripts.InsertTownCode $1
 echo Town Codes inserted into the tsv file $1
