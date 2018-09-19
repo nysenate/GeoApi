a8c2cd304c87ee9208969a52d199fe7055240672
Anthony Calabrese <aacal050@gmail.com>
2018-09-19 10:56:25 -0400

    Update path to include new Sage Version
    Sage has updated to 2.5.1 from 2.5 and In order for this script to run
    it needs to be using the correct version of SAGE

--- src/main/java/gov/nysenate/sage/scripts/StreetFinder/Scripts/GetTownCodes.sh
+++ src/main/java/gov/nysenate/sage/scripts/StreetFinder/Scripts/GetTownCodes.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 source config
-java -classpath "${pathToGeoApi}/GeoApi/target/classes/:${pathToGeoApi}/GeoApi/target/geoapi##2.5/WEB-INF/lib/*" gov.nysenate.sage.scripts.StreetFinder.Scripts.GetTownCodes
+java -classpath "${pathToGeoApi}/GeoApi/target/classes/:${pathToGeoApi}/GeoApi/target/geoapi##2.5.1/WEB-INF/lib/*" gov.nysenate.sage.scripts.StreetFinder.Scripts.GetTownCodes
 echo Town code file created
