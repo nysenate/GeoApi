42bf2b2b1171d53df7b2ab40efaf86e3a5f7e8a4
Anthony Calabrese <aacal050@gmail.com>
2018-10-24 15:54:01 -0400

    Improve script versioning
    Some of the scripts require all of the dependencies of SAGE to operate.
    This means as the sage version changes, the scripts will also need to
    point to the properly versioned war file. Rather than change of the
    scripts seperately, there is now one variable that needs to be changed
    in the config that handles this

--- src/main/java/gov/nysenate/sage/scripts/streetfinder/scripts/InsertTownCodes.sh
+++ src/main/java/gov/nysenate/sage/scripts/streetfinder/scripts/InsertTownCodes.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 source config
-java -classpath "${pathToGeoApi}/GeoApi/target/classes/:${pathToGeoApi}/GeoApi/target/geoapi##2.5.1/WEB-INF/lib/*" gov.nysenate.sage.scripts.streetfinder.scripts.InsertTownCode $1
+java -classpath "${pathToGeoApi}/GeoApi/target/classes/:${pathToGeoApi}/GeoApi/target/${version}/WEB-INF/lib/*" gov.nysenate.sage.scripts.streetfinder.scripts.InsertTownCode $1
 echo Town Codes inserted into the tsv file $1
