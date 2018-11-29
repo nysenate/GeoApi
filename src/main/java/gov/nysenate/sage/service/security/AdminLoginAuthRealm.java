4f51a7ac89e592f42473e655a84a5742ace65cda
Anthony Calabrese <aacal050@gmail.com>
2018-11-29 17:06:25 -0500

    Update runtime error message on missing admin user

--- src/main/java/gov/nysenate/sage/service/security/AdminLoginAuthRealm.java
+++ src/main/java/gov/nysenate/sage/service/security/AdminLoginAuthRealm.java
@@ -58,8 +58,7 @@ public class AdminLoginAuthRealm extends SageAuthorizingRealm
     @PostConstruct
     public void setup() throws RuntimeException {
         if (!adminUserAuth.checkAdminUser(defaultAdminName, defaultAdminPass)) {
-            logger.info("Default admin user not present in database.  " +
-                    "Creating default admin according to properties...");
+            logger.info("Default admin user not present in database, the SAGE database was built incorrectly");
             throw new RuntimeException("Missing Admin in DB");
         }
     }
