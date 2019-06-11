af8d42d501cce6ab6fd81ee5a50cbad0ec219216
aa29cala <aacal050@gmail.com>
2019-06-11 11:26:22 -0400

    Remove Location and state from update sql
    The location and state were causing the dev build to not update properly
    which is a huge issue. I had added these experimentally and these
    changes never hit prod. They have been removed to ensure updating the
    cache works properly again

--- src/main/java/gov/nysenate/sage/dao/provider/GeoCacheDao.java
+++ src/main/java/gov/nysenate/sage/dao/provider/GeoCacheDao.java
@@ -162,7 +162,7 @@ public class GeoCacheDao extends BaseDao
 
     private final static String SQL_UPDATE_CACHE_ENTRY = "update cache.geocache\n" +
             "set latlon = ST_GeomFromText(?), method = ?, quality = ?, zip4 = ?, updated = now()\n" +
-            "where bldgnum = ?  and street = ? and streettype = ? and predir = ? and postdir = ? and zip5 = ? and location = ? and state = ?;";
+            "where bldgnum = ?  and street = ? and streettype = ? and predir = ? and postdir = ? and zip5 = ? ;";
 
     /**
      * Saves any GeocodedAddress objects stored in the buffer into the database. The address is parsed into
@@ -205,9 +205,7 @@ public class GeoCacheDao extends BaseDao
                                             sa.getStreetType(),
                                             sa.getPreDir(),
                                             sa.getPostDir(),
-                                            sa.getZip5(),
-                                            sa.getLocation(),
-                                            sa.getState()
+                                            sa.getZip5()
                                             );
                                     if (logger.isTraceEnabled()) {
                                         logger.trace("Saved " + sa.toString() + " in cache.");
