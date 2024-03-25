74d4c30c9725b4f932c72d14ee76935bac48f301
JacobKeegan <jacobmkeegan@gmail.com>
2024-03-25 19:36:06 -0400

    Removed some voterfile-specific code in favor of more general methods.

--- src/main/java/gov/nysenate/sage/scripts/streetfinder/parsers/VoterFileParser.java
+++ src/main/java/gov/nysenate/sage/scripts/streetfinder/parsers/VoterFileParser.java
@@ -17,6 +17,35 @@ public class VoterFileParser extends BaseParser {
                 .addBuildingIndices(4).addStreetIndices(6, 7, 8).addType(CITY, 12).addType(ZIP, 13)
                 .addType(COUNTY, 23).addTypesInOrder(ELECTION, CLEG).addType(WARD, 27)
                 .addTypesInOrder(CONGRESSIONAL, SENATE, ASSEMBLY)
-                .addIdFunction((lineParts, lineNum) -> (long) Integer.parseInt(lineParts.get(45).replaceFirst("^NY", "")));
+                .addIdFunction((lineParts, lineNum) -> Long.parseLong(lineParts.get(45).replaceFirst("^NY", "")));
+    }
+
+    @Override
+    protected void addData(int lineNum, String... dataFields) {
+        if (dataFields[41].matches("[IP]")) {
+            return;
+        }
+        var nonStandardAddress = new NonStandardAddress(dataFields[11], dataFields[13]);
+        boolean emptyStandardAddress = dataFields[4].isEmpty() || dataFields[7].isEmpty();
+        if (!dataFields[11].isEmpty()) {
+            if (!emptyStandardAddress) {
+                // TODO: add problem
+            }
+            else {
+                var nonStAddr = new NonStandardAddress(dataFields[11], dataFields[13]);
+                if (nonStAddr.type().isValid()) {
+                    // TODO: add
+                } else {
+                    //TODO: set type
+                }
+            }
+        } else if (emptyStandardAddress) {
+            // TODO: no address type
+        }
+    }
+
+    @Override
+    public boolean isRangeData() {
+        return false;
     }
 }
--- src/main/java/gov/nysenate/sage/scripts/streetfinder/scripts/utils/VoterFileFieldType.java
+++
@@ -1,8 +0,0 @@
-package gov.nysenate.sage.scripts.streetfinder.scripts.utils;
-
-/**
- * A simple, useful enum to organize all the fields.
- */
-public enum VoterFileFieldType {
-    NAME, ADDRESS, DISTRICT, OTHER
-}
