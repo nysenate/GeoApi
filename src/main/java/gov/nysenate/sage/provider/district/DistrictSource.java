c241fe467e6c297382902520be94a650b8a0d4e4
JacobKeegan <jacobmkeegan@gmail.com>
2025-01-13 18:05:42 -0500

    Removed old, unused Geoserver code.

--- src/main/java/gov/nysenate/sage/provider/district/DistrictSource.java
+++ src/main/java/gov/nysenate/sage/provider/district/DistrictSource.java
@@ -3,5 +3,5 @@ package gov.nysenate.sage.provider.district;
 import gov.nysenate.sage.provider.geocode.DataSource;
 
 public enum DistrictSource implements DataSource {
-    SHAPEFILE, STREETFILE, GEOSERVER
+    SHAPEFILE, STREETFILE
 }
