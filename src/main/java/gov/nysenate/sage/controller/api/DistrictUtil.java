a1978c70c5863329067b20c663a34c80fce80887
JacobKeegan <jacobmkeegan@gmail.com>
2025-03-05 17:58:01 -0500

    Split out methods of combining DistrictInfo information.

--- src/main/java/gov/nysenate/sage/controller/api/DistrictUtil.java
+++ src/main/java/gov/nysenate/sage/controller/api/DistrictUtil.java
@@ -3,30 +3,46 @@ package gov.nysenate.sage.controller.api;
 import gov.nysenate.sage.model.district.DistrictInfo;
 import gov.nysenate.sage.model.district.DistrictType;
 
-import java.util.Collection;
 import java.util.List;
 
 public final class DistrictUtil {
     private DistrictUtil() {}
 
     /**
-     * Iterates over a list of DistrictInfo
+     * Returns a DistrictInfo without conflicts between codes.
      */
-    // TODO: fill in, or combine?
-    public static DistrictInfo consolidateDistrictInfo(Collection<DistrictInfo> districtInfoList) {
+    public static DistrictInfo getDistrictInfoWithoutConflicts(List<DistrictInfo> districtInfoList) {
         if (districtInfoList.isEmpty()) {
             return new DistrictInfo();
         }
-        DistrictInfo baseDistInfo = districtInfoList.iterator().next();
-        if (baseDistInfo == null) {
-            return new DistrictInfo();
-        }
+        DistrictInfo baseDistInfo = districtInfoList.get(0);
         for (DistrictType distType : DistrictType.values()) {
             String baseCode = baseDistInfo.getDistCode(distType);
-            List<String> codes = districtInfoList.stream().map(info -> info.getDistCode(distType)).toList();
-            if (codes.stream().anyMatch(code -> !isValidDistCode(code) || !baseCode.equals(code))) {
+            if (districtInfoList.stream().map(info -> info.getDistCode(distType))
+                    .anyMatch(code -> !isValidDistCode(code) || !baseCode.equals(code))) {
                 baseDistInfo.setDistCode(distType, null);
             }
+        }
+        return baseDistInfo;
+    }
+
+    /**
+     * Returns a DistrictInfo, filling in as many codes as possible.
+     * The first valid code in the List is used, if it exists.
+     */
+    public static DistrictInfo consolidateDistrictInfo(List<DistrictInfo> districtInfoList) {
+        if (districtInfoList.isEmpty()) {
+            return new DistrictInfo();
+        }
+        DistrictInfo baseDistInfo = districtInfoList.get(0);
+        for (DistrictType distType : DistrictType.values()) {
+            String baseCode = baseDistInfo.getDistCode(distType);
+            if (baseCode != null) {
+                continue;
+            }
+            districtInfoList.stream().skip(1).map(info -> info.getDistCode(distType))
+                    .filter(DistrictUtil::isValidDistCode).findFirst()
+                    .ifPresent(newCode -> baseDistInfo.setDistCode(distType, newCode));
         }
         return baseDistInfo;
     }
