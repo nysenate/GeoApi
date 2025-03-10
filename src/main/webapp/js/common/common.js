035d0e85d3b526bf4e11c0d0ed326497e041029b
JacobKeegan <jacobmkeegan@gmail.com>
2025-03-10 18:37:30 -0400

    Fixed usage of deprecated function.

--- src/main/webapp/js/common/common.js
+++ src/main/webapp/js/common/common.js
@@ -2,7 +2,7 @@
 var sageCommon = angular.module('sage-common', []);
 
 function initVerticalMenu() {
-    $(".top-method-header li a").click(function(event) {
+    $(".top-method-header li a").on("click", function(event) {
         if (!$(this).hasClass("active")) {
             $(".top-method-header li a.active").removeClass("active");
             $(this).addClass("active");
