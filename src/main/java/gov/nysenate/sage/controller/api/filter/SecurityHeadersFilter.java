4643d182240d64b8fdc114cbeaeca917e2b66cca
Kevin Caseiras <kcaseiras@gmail.com>
2026-04-30 15:07:44 -0400

    Update security headers imports for spring6

--- src/main/java/gov/nysenate/sage/controller/api/filter/SecurityHeadersFilter.java
+++ src/main/java/gov/nysenate/sage/controller/api/filter/SecurityHeadersFilter.java
@@ -1,9 +1,9 @@
 package gov.nysenate.sage.controller.api.filter;
 
+import jakarta.servlet.*;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.stereotype.Component;
 
-import javax.servlet.*;
-import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 /**
