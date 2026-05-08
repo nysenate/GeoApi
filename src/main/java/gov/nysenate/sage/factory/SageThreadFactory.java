6c04f6fb60e31ba93b2467d544312e3d710dee18
JacobKeegan <jacobmkeegan@gmail.com>
2026-05-08 17:55:14 -0400

    Fixed a logging issue. Upgraded to Java 21.

--- src/main/java/gov/nysenate/sage/factory/SageThreadFactory.java
+++ src/main/java/gov/nysenate/sage/factory/SageThreadFactory.java
@@ -1,24 +1,23 @@
 package gov.nysenate.sage.factory;
 
+import org.jspecify.annotations.NonNull;
+
 import java.util.concurrent.ThreadFactory;
 import java.util.concurrent.atomic.AtomicInteger;
 
-public class SageThreadFactory implements ThreadFactory
-{
+public class SageThreadFactory implements ThreadFactory {
     private static final AtomicInteger poolNumber = new AtomicInteger(1);
     private final ThreadGroup group;
     private final AtomicInteger threadNumber = new AtomicInteger(1);
     private final String namePrefix;
 
     public SageThreadFactory(String prefix) {
-        SecurityManager s = System.getSecurityManager();
-        group = (s != null) ? s.getThreadGroup() :
-                Thread.currentThread().getThreadGroup();
-        namePrefix = prefix + "-" + poolNumber.getAndIncrement() + "-thread-";
+        this.group = Thread.currentThread().getThreadGroup();
+        this.namePrefix = prefix + "-" + poolNumber.getAndIncrement() + "-thread-";
     }
 
     @Override
-    public Thread newThread(Runnable r) {
+    public Thread newThread(@NonNull Runnable r) {
         Thread t = new Thread(group, r, namePrefix + threadNumber.getAndIncrement(), 0);
         if (t.isDaemon())
             t.setDaemon(false);
