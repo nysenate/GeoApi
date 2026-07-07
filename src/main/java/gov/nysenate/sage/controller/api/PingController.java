235967a3de8bbef2722f81bf6272bf4497858a16
JacobKeegan <jacobmkeegan@gmail.com>
2026-07-07 00:37:52 -0400

    Narrowed the ping endpoint.

--- src/main/java/gov/nysenate/sage/controller/api/PingController.java
+++ src/main/java/gov/nysenate/sage/controller/api/PingController.java
@@ -2,13 +2,14 @@ package gov.nysenate.sage.controller.api;
 
 import gov.nysenate.sage.client.response.base.BaseResponse;
 import gov.nysenate.sage.model.result.ResultStatus;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/ping")
 public class PingController {
-    @RequestMapping("")
+    @GetMapping("")
     public BaseResponse ping() {
         return new BaseResponse(ResultStatus.SUCCESS);
     }
