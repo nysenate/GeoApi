e8df04c336117071af7afc6846a8b8ceea9ab578
JacobKeegan <jacobmkeegan@gmail.com>
2026-07-20 22:52:43 -0400

    Fixed an issue that could potentially cause the number of batch results returned to not match the input.

--- src/main/java/gov/nysenate/sage/client/response/base/BatchResponse.java
+++ src/main/java/gov/nysenate/sage/client/response/base/BatchResponse.java
@@ -1,7 +1,6 @@
 package gov.nysenate.sage.client.response.base;
 
 import gov.nysenate.sage.model.result.ResultStatus;
-import gov.nysenate.sage.util.NonnullList;
 
 import javax.annotation.Nonnull;
 import java.util.List;
@@ -13,7 +12,7 @@ public class BatchResponse<T extends BaseResponse> extends BaseResponse {
 
     public <I> BatchResponse(List<I> inputList, Function<I, T> mapper) {
         super(ResultStatus.SUCCESS);
-        this.results = NonnullList.of(inputList).stream().map(mapper).toList();
+        this.results = inputList.stream().map(input -> input == null ? null : mapper.apply(input)).toList();
     }
 
     @Nonnull
